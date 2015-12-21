55b81c8f5fb38909ce0560d94cb20827f9bee795
Jerome Lambourg <lambourg@adacore.com>
2015-12-21 14:38:00 +0100

    Rename STM32_Board to STM32.Board.
    The board was the only package not in the STM32 package hierarchy, while this
    makes no sense to keep it out of this hierarchy.

--- ARM/STMicro/STM32/boards/stm32f429_discovery/stm32-board.adb
+++ ARM/STMicro/STM32/boards/stm32f429_discovery/stm32-board.adb
@@ -30,100 +30,67 @@
 --                                                                          --
 --  This file is based on:                                                  --
 --                                                                          --
---   @file    stm32f429i_discovery.h                                        --
+--   @file    stm32f429i_discovery.c                                        --
 --   @author  MCD Application Team                                          --
 --   @version V1.1.0                                                        --
 --   @date    19-June-2014                                                  --
---   @brief   This file contains definitions for STM32F429I-Discovery Kit   --
---            LEDs, push-buttons hardware resources.                        --
+--   @brief  This file provides set of firmware functions to manage Leds    --
+--           and push-button available on STM32F429I-Discovery Kit from     --
+--           STMicroelectronics.                                            --
 --                                                                          --
 --   COPYRIGHT(c) 2014 STMicroelectronics                                   --
 ------------------------------------------------------------------------------
 
---  This file provides declarations for devices on the STM32F429 Discovery kits
---  manufactured by ST Microelectronics.
+package body STM32.Board is
 
-with STM32.Device;  use STM32.Device;
+   ------------------
+   -- All_LEDs_Off --
+   ------------------
 
-with STM32.GPIO;    use STM32.GPIO;
-with STM32.ADC;     use STM32.ADC;
-with STM32.L3DG20;  use STM32.L3DG20;
+   procedure All_LEDs_Off is
+   begin
+      Clear (ALL_LEDs);
+   end All_LEDs_Off;
 
-with Ada.Interrupts.Names;  use Ada.Interrupts;
+   -----------------
+   -- All_LEDs_On --
+   -----------------
 
-use STM32;  -- for base addresses
+   procedure All_LEDs_On is
+   begin
+      Set (ALL_LEDs);
+   end All_LEDs_On;
 
-package STM32_Board is
+   ---------------------
+   -- Initialize_LEDs --
+   ---------------------
 
-   pragma Elaborate_Body;
+   procedure Initialize_LEDs is
+      Conf : GPIO_Port_Configuration;
+   begin
+      Enable_Clock (ALL_LEDs);
 
-   subtype User_LED is GPIO_Point;
+      Conf.Mode        := Mode_Out;
+      Conf.Output_Type := Push_Pull;
+      Conf.Speed       := Speed_100MHz;
+      Conf.Resistors   := Floating;
 
-   Green : User_LED renames PG13;
-   Red   : User_LED renames PG14;
+      Configure_IO (All_LEDs, Conf);
+   end Initialize_LEDs;
 
-   LED3  : User_LED renames Green;
-   LED4  : User_LED renames Red;
+   --------------------------------
+   -- Configure_User_Button_GPIO --
+   --------------------------------
 
-   All_LEDs  : constant GPIO_Points := LED3 & LED4;
+   procedure Configure_User_Button_GPIO is
+      Config : GPIO_Port_Configuration;
+   begin
+      Enable_Clock (User_Button_Point);
 
-   procedure Initialize_LEDs;
-   --  MUST be called prior to any use of the LEDs
+      Config.Mode := Mode_In;
+      Config.Resistors := Floating;
 
-   procedure Turn_On (This : User_LED)
-     renames STM32.GPIO.Set;
-   procedure Turn_Off (This : User_LED)
-     renames STM32.GPIO.Clear;
+      Configure_IO (User_Button_Point, Config);
+   end Configure_User_Button_GPIO;
 
-   procedure All_LEDs_Off with Inline;
-   procedure All_LEDs_On  with Inline;
-   procedure Toggle_LEDs (These : GPIO_Points)
-     renames STM32.GPIO.Toggle;
-
-   Gyro : Three_Axis_Gyroscope;
-
-   --  GPIO Pins for FMC
-   FMC_A : constant GPIO_Points :=
-             (PF0, PF1, PF2, PF3, PF4, PF5,
-              PF12, PF13, PF14, PF15, PG0, PG1);
-
-   FMC_D : constant GPIO_Points :=
-             (PD14, PD15, PD0, PD1, PE7, PE8, PE9, PE10, PE11, PE12,
-              PE13, PE14, PE15, PD8, PD9, PD10);
-
-   FMC_SDNWE     : GPIO_Point renames PH5;
-   FMC_SDNRAS    : GPIO_Point renames PF11;
-   FMC_SDNCAS    : GPIO_Point renames PG15;
-   FMC_SDCLK     : GPIO_Point renames PG8;
-   FMC_BA0       : GPIO_Point renames PG4;
-   FMC_BA1       : GPIO_Point renames PG5;
-   FMC_SDNE0     : GPIO_Point renames PH3;
-   FMC_SDCKE0    : GPIO_Point renames PC3;
-   FMC_NBL0      : GPIO_Point renames PE0;
-   FMC_NBL1      : GPIO_Point renames PE1;
-
-   SDRAM_PINS    : constant GPIO_Points :=
-                     GPIO_Points'
-                       (FMC_SDNWE,
-                        FMC_SDNRAS,
-                        FMC_SDNCAS,
-                        FMC_SDCLK,
-                        FMC_BA0,
-                        FMC_BA1,
-                        FMC_SDNE0,
-                        FMC_SDCKE0,
-                        FMC_NBL0,
-                        FMC_NBL1) &
-                     FMC_A & FMC_D;
-
-   --  User button
-
-   User_Button_Point     : GPIO_Point renames PA0;
-   User_Button_Interrupt : constant Interrupt_Id := Names.EXTI0_Interrupt;
-
-   procedure Configure_User_Button_GPIO;
-   --  Configures the GPIO port/pin for the blue user button. Sufficient
-   --  for polling the button, and necessary for having the button generate
-   --  interrupts.
-
-end STM32_Board;
+end STM32.Board;
