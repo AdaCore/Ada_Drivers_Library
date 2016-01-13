c13da8b4c21cf3f7eae532a7b91b9594a12efb34
pat-rogers <rogers@adacore.com>
2016-01-12 22:31:28 -0600

    corrected years in copyright date

--- ARM/STMicro/STM32/drivers/stm32f4-gpio.ads
+++ ARM/STMicro/STM32/drivers/stm32f4-gpio.ads
@@ -1,6 +1,6 @@
 ------------------------------------------------------------------------------
 --                                                                          --
---                    Copyright (C) 2015, AdaCore                           --
+--                  Copyright (C) 2015-2016, AdaCore                        --
 --                                                                          --
 --  Redistribution and use in source and binary forms, with or without      --
 --  modification, are permitted provided that the following conditions are  --
