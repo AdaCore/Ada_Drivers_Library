7b2cce8c8453488d27d0cb9c9a458e232d9b1bdf
Jerome Lambourg <lambourg@adacore.com>
2016-02-19 15:28:05 +0100

    Add support for the Hershey fonts.
    This allows vector font drawings.

--- examples/common/gui/bitmapped_drawing.adb
+++ examples/common/gui/bitmapped_drawing.adb
@@ -1,31 +1,23 @@
 ------------------------------------------------------------------------------
+--                        Bareboard drivers examples                        --
 --                                                                          --
---                    Copyright (C) 2015, AdaCore                           --
+--                     Copyright (C) 2015-2016, AdaCore                     --
 --                                                                          --
---  Redistribution and use in source and binary forms, with or without      --
---  modification, are permitted provided that the following conditions are  --
---  met:                                                                    --
---     1. Redistributions of source code must retain the above copyright    --
---        notice, this list of conditions and the following disclaimer.     --
---     2. Redistributions in binary form must reproduce the above copyright --
---        notice, this list of conditions and the following disclaimer in   --
---        the documentation and/or other materials provided with the        --
---        distribution.                                                     --
---     3. Neither the name of STMicroelectronics nor the names of its       --
---        contributors may be used to endorse or promote products derived   --
---        from this software without specific prior written permission.     --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
 --                                                                          --
---   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    --
---   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      --
---   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR  --
---   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT   --
---   HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, --
---   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       --
---   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  --
---   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  --
---   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT    --
---   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  --
---   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
 --                                                                          --
 ------------------------------------------------------------------------------
 
@@ -181,6 +173,153 @@ package body Bitmapped_Drawing is
             Background);
          Count := Count + 1;
       end loop;
+   end Draw_String;
+
+   -----------------
+   -- Draw_String --
+   -----------------
+
+   procedure Draw_String
+     (Buffer     : Graphics_Buffer;
+      Start      : Point;
+      Msg        : String;
+      Font       : Hershey_Font;
+      Height     : Natural;
+      Bold       : Boolean;
+      Foreground : DMA2D_Color;
+      Fast       : Boolean := True)
+   is
+      procedure Internal_Draw_Line
+        (X0, Y0, X1, Y1 : Natural;
+         Width          : Positive)
+      is
+      begin
+         Draw_Line (Buffer,
+                    (X0, Y0),
+                    (X1, Y1),
+                    Foreground,
+                    Width,
+                    Fast => Fast);
+      end Internal_Draw_Line;
+
+      procedure Draw_Glyph is new Hershey_Fonts.Draw_Glyph
+        (Internal_Draw_Line);
+
+      Current : Point := Start;
+
+   begin
+      for C of Msg loop
+         exit when Current.X > Buffer.Width;
+         Draw_Glyph
+           (Fnt    => Font,
+            C      => C,
+            X      => Current.X,
+            Y      => Current.Y,
+            Height => Height,
+            Bold   => Bold);
+      end loop;
+   end Draw_String;
+
+   -----------------
+   -- Draw_String --
+   -----------------
+
+   procedure Draw_String
+     (Buffer     : Graphics_Buffer;
+      Area       : Rect;
+      Msg        : String;
+      Font       : Hershey_Font;
+      Bold       : Boolean;
+      Outline    : Boolean;
+      Foreground : Graphics_Color;
+      Fast       : Boolean := True)
+   is
+      Length  : constant Natural :=
+                  Hershey_Fonts.Strlen (Msg, Font, Area.Height);
+      Ratio   : Float;
+      Current : Point := (0, 0);
+      Prev    : Unsigned_32;
+      FG      : constant Unsigned_32 :=
+                  DMA2D_Color_To_Word (Buffer, Foreground);
+      Blk     : constant Unsigned_32 :=
+                  DMA2D_Color_To_Word (Buffer, Black);
+
+      procedure Internal_Draw_Line
+        (X0, Y0, X1, Y1 : Natural;
+         Width          : Positive)
+      is
+      begin
+         Draw_Line (Buffer,
+                    (Area.Position.X + Natural (Float (X0) * Ratio),
+                     Area.Position.Y + Y0),
+                    (Area.Position.X + Natural (Float (X1) * Ratio),
+                     Area.Position.Y + Y1),
+                    Foreground,
+                    Width,
+                    Fast);
+      end Internal_Draw_Line;
+
+      procedure Draw_Glyph is new Hershey_Fonts.Draw_Glyph
+        (Internal_Draw_Line);
+
+   begin
+      if Length > Area.Width then
+         Ratio := Float (Area.Width) / Float (Length);
+      else
+         Ratio := 1.0;
+         Current.X := (Area.Width - Length) / 2;
+      end if;
+
+      for C of Msg loop
+         Draw_Glyph
+           (Fnt    => Font,
+            C      => C,
+            X      => Current.X,
+            Y      => Current.Y,
+            Height => Area.Height,
+            Bold   => Bold);
+      end loop;
+
+      if Outline and then Area.Height > 40 then
+         for Y in Area.Position.Y + 1 .. Area.Position.Y + Area.Height loop
+            Prev := Get_Pixel (Buffer, (Area.Position.X, Y));
+            if Prev = FG then
+               Put_Pixel (Buffer, (Area.Position.X, Y), Black);
+            end if;
+
+            for X in Area.Position.X + 1 .. Area.Position.X + Area.Width loop
+               declare
+                  Col : constant Unsigned_32 := Get_Pixel (Buffer, (X, Y));
+                  Top : constant Unsigned_32 := Get_Pixel (Buffer, (X, Y - 1));
+               begin
+
+                  if Prev /= FG
+                    and then Col = FG
+                  then
+                     Put_Pixel (Buffer, (X, Y), Blk);
+
+                  elsif Prev = FG
+                    and then Col /= FG
+                  then
+                     Put_Pixel (Buffer, (X - 1, Y), Blk);
+
+                  elsif Top /= FG
+                    and then Top /= Blk
+                    and then Col = FG
+                  then
+                     Put_Pixel (Buffer, (X, Y), Blk);
+
+                  elsif Top = FG
+                    and then Col /= FG
+                  then
+                     Put_Pixel (Buffer, (X, Y - 1), Blk);
+                  end if;
+
+                  Prev := Col;
+               end;
+            end loop;
+         end loop;
+      end if;
    end Draw_String;
 
    --  http://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm#Ada
--- examples/common/gui/bitmapped_drawing.ads
+++ examples/common/gui/bitmapped_drawing.ads
@@ -1,69 +1,33 @@
 ------------------------------------------------------------------------------
+--                        Bareboard drivers examples                        --
 --                                                                          --
---                    Copyright (C) 2015, AdaCore                           --
+--                     Copyright (C) 2015-2016, AdaCore                     --
 --                                                                          --
---  Redistribution and use in source and binary forms, with or without      --
---  modification, are permitted provided that the following conditions are  --
---  met:                                                                    --
---     1. Redistributions of source code must retain the above copyright    --
---        notice, this list of conditions and the following disclaimer.     --
---     2. Redistributions in binary form must reproduce the above copyright --
---        notice, this list of conditions and the following disclaimer in   --
---        the documentation and/or other materials provided with the        --
---        distribution.                                                     --
---     3. Neither the name of STMicroelectronics nor the names of its       --
---        contributors may be used to endorse or promote products derived   --
---        from this software without specific prior written permission.     --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
 --                                                                          --
---   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    --
---   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      --
---   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR  --
---   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT   --
---   HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, --
---   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       --
---   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  --
---   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  --
---   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT    --
---   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  --
---   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
 --                                                                          --
 ------------------------------------------------------------------------------
 
 --  This package provides routines for drawing shapes, characters, and strings
---  on a bit-mapped device. Any bit-mapped device will suffice that supplies a
---  routine to set a pixel, as shown by the formal subprogram.
---
---  For example, you can use this package in combination with the low-level LCD
---  component driver, the STM32F4.STM32F4.ILI9341, as shown in the following:
---
---     package LCD_Drawing is new Bitmapped_Drawing
---       (Color     => STM32F4.ILI9341.Colors,
---        Set_Pixel => STM32F4.ILI9341.Set_Pixel);
---
---  Such an instantiation would then allow you to write on the LCD:
---
---     use LCD_Drawing;
---
---  begin
---     Draw_String
---       (Start      => (0, 0),
---        Msg        => Chars,
---        Font       => Font16x24,
---        Foreground => White,
---        Background => Black);
---
---     Draw_Circle
---       (Center => (40, 220),
---        Radius => 20,
---        Hue    => Blue,
---        Fill   => True);
---
---  There is no control over colors or screen orientation. If that is required,
---  see the LCD_Std_Out package.
+--  on a bit-mapped device or graphical buffer.
 
-with Interfaces;  use Interfaces;
-with BMP_Fonts;   use BMP_Fonts;
-with STM32.DMA2D; use STM32.DMA2D;
+with Interfaces;    use Interfaces;
+with BMP_Fonts;     use BMP_Fonts;
+with Hershey_Fonts; use Hershey_Fonts;
+with STM32.DMA2D;   use STM32.DMA2D;
 
 package Bitmapped_Drawing is
 
@@ -203,8 +167,28 @@ package Bitmapped_Drawing is
       Start      : Point;
       Msg        : String;
       Font       : BMP_Font;
+      Foreground : Graphics_Color;
+      Background : Graphics_Color);
+
+   procedure Draw_String
+     (Buffer     : Graphics_Buffer;
+      Start      : Point;
+      Msg        : String;
+      Font       : Hershey_Font;
+      Height     : Natural;
+      Bold       : Boolean;
       Foreground : DMA2D_Color;
-      Background : DMA2D_Color);
+      Fast       : Boolean := True);
+
+   procedure Draw_String
+     (Buffer     : Graphics_Buffer;
+      Area       : Rect;
+      Msg        : String;
+      Font       : Hershey_Font;
+      Bold       : Boolean;
+      Outline    : Boolean;
+      Foreground : Graphics_Color;
+      Fast       : Boolean := True);
 
 end Bitmapped_Drawing;
 
