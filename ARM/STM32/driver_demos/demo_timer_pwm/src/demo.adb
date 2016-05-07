fda12772e2c9dd968cb601344dd301bfcc9a2efc
pat-rogers <rogers@adacore.com>
2016-05-07 11:21:06 -0500

    Updated to make calls directly to timer in order to implement PWM, to show how that is done and to have a low-level direct test of the functionality.

--- ARM/STM32/driver_demos/demo_timer_pwm/src/demo.adb
+++ ARM/STM32/driver_demos/demo_timer_pwm/src/demo.adb
@@ -34,43 +34,63 @@
 --  iteratively, for as long as the application runs. In effect the LED light
 --  waxes and wanes. See http://visualgdb.com/tutorials/arm/stm32/fpu/ for the
 --  inspiration.
+--
+--  The demo uses the timer directly to achieve the effect of
+--  pulse-width-modulation, but note that there is a PWM package that does
+--  this directly, hiding much of the details that are explicit here.
 
 with Last_Chance_Handler;  pragma Unreferenced (Last_Chance_Handler);
 
 with STM32.Board;  use STM32.Board;
 with STM32.Device; use STM32.Device;
+with HAL;          use HAL;
 
-with STM32.PWM;    use STM32.PWM;
 with STM32.GPIO;   use STM32.GPIO;
 with STM32.Timers; use STM32.Timers;
 
 procedure Demo is
 
-   Output : PWM_Modulator;
-
-   Output_Timer : Timer renames Timer_4;
+   Period : constant := 1000;
 
    Output_Channel : constant Timer_Channel := Channel_1;
    --  The LED driven by this example is determined by the channel selected.
    --  That is so because each channel of Timer_4 is connected to a specific
    --  LED in the alternate function configuration on this board. We will
-   --  initialize one of the LEDs to be in the AF mode for Timer_4. The
+   --  initialize all of the LEDs to be in the AF mode for Timer_4. The
    --  particular channel selected is completely arbitrary, as long as the
    --  selected GPIO port/pin for the LED matches the selected channel.
-
+   --
    --  Channel_1 is connected to the green LED.
    --  Channel_2 is connected to the orange LED.
    --  Channel_3 is connected to the red LED.
    --  Channel_4 is connected to the blue LED.
 
-   Output_Point : constant GPIO_Point := Green;
-   --  This must match the GPIO port/pin for the selected Output_Channel value!
+   --------------------
+   -- Configure_LEDs --
+   --------------------
+
+   procedure Configure_LEDs;
+
+   procedure Configure_LEDs is
+      Configuration : GPIO_Port_Configuration;
+   begin
+      Enable_Clock (GPIO_D);
+
+      Configuration.Mode        := Mode_AF;  -- essential
+      Configuration.Output_Type := Push_Pull;
+      Configuration.Speed       := Speed_50MHz;
+      Configuration.Resistors   := Floating;
+
+      Configure_IO (All_LEDs, Configuration);
+   end Configure_LEDs;
 
    --  The SFP run-time library for these boards is intended for certified
    --  environments and so does not contain the full set of facilities defined
    --  by the Ada language. The elementary functions are not included, for
    --  example. In contrast, the Ravenscar "full" run-times do have these
    --  functions.
+
+   function Sine (Input : Long_Float) return Long_Float;
 
    --  Therefore there are four choices: 1) use the "ravescar-full-stm32f4"
    --  runtime library, 2) pull the sources for the language-defined elementary
@@ -81,7 +101,6 @@ procedure Demo is
 
    --  In this demonstration we roll our own approximation to the sine function
    --  so that it doesn't matter which runtime library is used.
-   function Sine (Input : Long_Float) return Long_Float;
 
    function Sine (Input : Long_Float) return Long_Float is
       Pi : constant Long_Float := 3.14159_26535_89793_23846;
@@ -101,29 +120,48 @@ procedure Demo is
    --  that value, thus the waxing/waning effect.
 
 begin
-   Initialise_PWM_Modulator
-     (Output,
-      Requested_Frequency => 30_000.0, -- arbitrary
-      PWM_Timer           => Output_Timer'Access,
-      PWM_AF              => GPIO_AF_TIM4);
+   Configure_LEDs;
 
-   Attach_PWM_Channel
-     (Output,
-      Output_Channel,
-      Output_Point);
+   Enable_Clock (Timer_4);
+
+   Reset (Timer_4);
+
+   Configure
+     (Timer_4,
+      Prescaler     => 1,
+      Period        => Period,
+      Clock_Divisor => Div1,
+      Counter_Mode  => Up);
+
+   Configure_Channel_Output
+     (Timer_4,
+      Channel  => Output_Channel,
+      Mode     => PWM1,
+      State    => Enable,
+      Pulse    => 0,
+      Polarity => High);
+
+   Set_Autoreload_Preload (Timer_4, True);
+
+   Configure_Alternate_Function (All_LEDs, AF => GPIO_AF_TIM4);
+   --  Note we configured the LEDs to be in the AF mode in Configure_LEDs
+
+   Enable_Channel (Timer_4, Output_Channel);
+
+   Enable (Timer_4);
 
    declare
       use STM32;
       Arg       : Long_Float := 0.0;
-      Value     : Percentage;
+      Pulse     : Short;
       Increment : constant Long_Float := 0.00003;
       --  The Increment value controls the rate at which the brightness
       --  increases and decreases. The value is more or less arbitrary, but
       --  note that the effect of optimization is observable.
    begin
       loop
-         Value := Percentage (50.0 * (1.0 + Sine (Arg)));
-         Set_Duty_Cycle (Output, Output_Channel, Value);
+         Pulse := Short (Long_Float (Period / 2) * (1.0 + Sine (Arg)));
+         Set_Compare_Value (Timer_4, Output_Channel, Pulse);
          Arg := Arg + Increment;
       end loop;
    end;
