0075053501296c22543d7e434e34b1921e250544
pat-rogers <rogers@adacore.com>
2016-05-08 14:28:30 -0500

    Update for new architecture. Add comments and new constants making it clearer how to experiment with causing the watchdog timeout to occur.

--- ARM/STM32/driver_demos/demo_independent_watchdog/src/demo_iwdg.adb
+++ ARM/STM32/driver_demos/demo_independent_watchdog/src/demo_iwdg.adb
@@ -51,16 +51,51 @@ with Ada.Real_Time; use Ada.Real_Time;
 with STM32.Board;   use STM32.Board;
 
 procedure Demo_IWDG is
+
+   Watchdog_Count : constant := 4095;  -- arbitrary
+   --  The clock driving the watchdog downcounter is approximately 32KHz, so
+   --  dividing by 32 gives about 1 millisecond per count. Thus the count gives
+   --  approximately that many milliseconds before the system reset interrupt
+   --  is triggered. Feel free to experiment with this value.
+
+   Watchdog_Count_Offset : constant := -90; -- arbitrary
+   --  The entirely arbitrary amount to add/subtract to the watchdog count, ie
+   --  the time that causes a board reset, in order to experiment with causing
+   --  the reset to occur. This value is added to the Watchdog_Count and the
+   --  result passed to Do_Work, thereby controlling whether the watchdog timer
+   --  trips or not. Feel free to experiment with this value.
+
+   Work_Time : constant := Watchdog_Count + Watchdog_Count_Offset;
+   --  The value passed to Do_Work which determines the amount of time taken by
+   --  that call. By changing the value of Watchdog_Count_Offset you can cause
+   --  the reset to occur, or not.  Note that optimization will have influence.
+
+   procedure Do_Work (Time_Required : Time_Span);
+   --  A simple abstraction for some amount of work to be done while the
+   --  watchdog is running. The Time_Required parameter controls how long the
+   --  "work" should take, so that you can experiment with causing the reset
+   --  to occur by giving it a value close to the watchdog timeout interval.
+
+   procedure Do_Work (Time_Required : Time_Span) is
+      Interval : constant Time_Span := Time_Required / 4;
+      --  We divide so that we can blink more often
+   begin
+      Turn_On (Green);
+      delay until Clock + Interval;
+      Turn_Off (Green);
+      delay until Clock + Interval;
+      Turn_On (Green);
+      delay until Clock + Interval;
+      Turn_Off (Green);
+      delay until Clock + Interval;
+   end Do_Work;
+
 begin
    Initialize_LEDs;
 
-   Initialize_Watchdog (Prescaler => Divider_32, Count => 4095);
-   --  The clock driving the watchdog downcounter is approximately 32KHz, so
-   --  dividing by 32 gives about 1 millisecond per count. Thus an initial
-   --  (and reloaded) count of 4095 gives approximately that many milliseconds
-   --  (i.e., about 4.1 seconds) before the system reset interrupt is
-   --  triggered. Feel free to experiment with these values...
+   Initialize_Watchdog (Prescaler => Divider_32, Count => Watchdog_Count);
 
+   --  Indicate initial excution, but also indicate board (program) reset
    for K in 1 .. 3 loop
       All_LEDs_On;
       delay until Clock + Milliseconds (200);
@@ -69,12 +104,11 @@ begin
    end loop;
 
    Start_Watchdog;
-
    loop
-      Turn_On (Green);
-      delay until Clock + Milliseconds (500);
-      Turn_Off (Green);
-      delay until Clock + Milliseconds (500);
+      Do_Work (Time_Required => Milliseconds (Work_Time));
+      --  If Do_Work does not return in time so that the call to reset the
+      --  watchdog can occur, the board will reset and the program will
+      --  restart.
 
       Reset_Watchdog;
    end loop;
