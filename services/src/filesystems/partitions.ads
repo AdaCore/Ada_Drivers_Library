b8c2ac68d7c43604d358883e5813a611bbf850cd
Fabien Chouteau <chouteau@adacore.com>
2016-08-29 23:07:31 +0200

    Partitions: Change Parition_Kind to Unsigned_8
    Enum would be better but it means we have to give a name to all 256
    possible values.

--- services/src/filesystems/partitions.ads
+++ services/src/filesystems/partitions.ads
@@ -4,19 +4,20 @@ with Interfaces; use Interfaces;
 
 package Partitions is
 
-   type Partition_Kind is
-     (Empty_Partition,
-      Fat12_Parition,
-      Fat16_Parition,
-      Extended_Parition,
-      Fat16B_Parition,
-      NTFS_Partition,
-      Fat32_CHS_Parition,
-      Fat32_LBA_Parition,
-      Fat16B_LBA_Parition,
-      Extended_LBA_Parition,
-      Linux_Partition)
-     with Size => 8;
+   type Partition_Kind is new Unsigned_8;
+
+   Empty_Partition       : constant Partition_Kind := 16#00#;
+   Fat12_Parition        : constant Partition_Kind := 16#01#;
+   Fat16_Parition        : constant Partition_Kind := 16#04#;
+   Extended_Parition     : constant Partition_Kind := 16#05#;
+   Fat16B_Parition       : constant Partition_Kind := 16#06#;
+   NTFS_Partition        : constant Partition_Kind := 16#07#;
+   Fat32_CHS_Parition    : constant Partition_Kind := 16#0B#;
+   Fat32_LBA_Parition    : constant Partition_Kind := 16#0C#;
+   Fat16B_LBA_Parition   : constant Partition_Kind := 16#0E#;
+   Extended_LBA_Parition : constant Partition_Kind := 16#0F#;
+   Linux_Swap_Partition  : constant Partition_Kind := 16#82#;
+   Linux_Partition       : constant Partition_Kind := 16#83#;
 
    subtype Logical_Block_Address is Unsigned_32;
 
@@ -46,19 +47,4 @@ package Partitions is
 
    function Is_Valid (P_Entry : Partition_Entry) return Boolean is
      (P_Entry.Status = 16#00# or else P_Entry.Status = 16#80#);
-
-private
-
-   for Partition_Kind use
-     (Empty_Partition       => 16#00#,
-      Fat12_Parition        => 16#01#,
-      Fat16_Parition        => 16#04#,
-      Extended_Parition     => 16#05#,
-      Fat16B_Parition       => 16#06#,
-      NTFS_Partition        => 16#07#,
-      Fat32_CHS_Parition    => 16#0B#,
-      Fat32_LBA_Parition    => 16#0C#,
-      Fat16B_LBA_Parition   => 16#0E#,
-      Extended_LBA_Parition => 16#0F#,
-      Linux_Partition       => 16#83#);
 end Partitions;
