e8aabf251214a38323b7712cf98fb697cf7fc0c4
Jerome Lambourg <lambourg@adacore.com>
2016-09-22 12:12:32 +0200

    Add the CS43L22 Audio driver (as found in the stm32f479-disco board).

---
+++ components/src/audio/CS43L22/cs43l22.adb
@@ -0,0 +1,282 @@
+package body CS43L22 is
+
+   ---------------
+   -- I2C_Write --
+   ---------------
+
+   procedure I2C_Write (This  : in out CS43L22_Device;
+                        Reg   : Byte;
+                        Value : Byte)
+   is
+      Status : I2C_Status with Unreferenced;
+
+   begin
+      This.Port.Mem_Write
+        (Addr          => CS43L22_I2C_Addr,
+         Mem_Addr      => Unsigned_16 (Reg),
+         Mem_Addr_Size => Memory_Size_8b,
+         Data          => (1 => Value),
+         Status        => Status);
+   end I2C_Write;
+
+   --------------
+   -- I2C_Read --
+   --------------
+
+   function I2C_Read (This : in out CS43L22_Device;
+                      Reg : Byte)
+                      return Byte
+   is
+      Status : I2C_Status;
+      Data   : I2C_Data (1 .. 1);
+
+   begin
+      This.Port.Mem_Read
+        (Addr          => CS43L22_I2C_Addr,
+         Mem_Addr      => Unsigned_16 (Reg),
+         Mem_Addr_Size => Memory_Size_8b,
+         Data          => Data,
+         Status        => Status);
+      return Data (1);
+   end I2C_Read;
+
+   ----------
+   -- Init --
+   ----------
+
+   procedure Init
+     (This      : in out CS43L22_Device;
+      Output    : Output_Device;
+      Volume    : Volume_Level;
+      Frequency : Audio_Frequency)
+   is
+      pragma Unreferenced (Frequency);
+   begin
+      --  Codec set to power OFF
+      This.I2C_Write (CS43L22_REG_POWER_CTL1, 16#01#);
+
+      --  Save output device for mute ON/OFF procedure
+      This.Set_Output_Mode (Output);
+
+      --  Clock configuration: auto detection
+      This.I2C_Write (CS43L22_REG_CLOCKING_CTL, 16#81#);
+
+      --  Set the Slave Mode and the audio Standard
+      This.I2C_Write (CS43L22_REG_INTERFACE_CTL1, 16#04#);
+
+      --  Set the Master volume */
+      This.Set_Volume (Volume);
+
+      --  If the Speaker is enabled, set the Mono mode and volume attenuation
+      --  level
+      if (Output /= Headphone) then
+         --  Set the Speaker Mono mode
+         This.I2C_Write (CS43L22_REG_PLAYBACK_CTL2, 16#06#);
+
+         --  Set the Speaker attenuation level
+         This.I2C_Write (CS43L22_REG_SPEAKER_A_VOL, 16#00#);
+         This.I2C_Write (CS43L22_REG_SPEAKER_B_VOL, 16#00#);
+      end if;
+
+      --  Additional configuration for the CODEC. These configurations are
+      --  done to reduce the time needed for the Codec to power off. If these
+      --  configurations are removed, then a long delay should be added between
+      --  powering off the Codec and switching off the I2S peripheral MCLK
+      --  clock (which is the operating clock for Codec).
+
+      --  If this delay is not inserted, then the codec will not shut down
+      --  properly and it results in high noise after shut down.
+
+      --  Disable the analog soft ramp
+      This.I2C_Write (CS43L22_REG_ANALOG_ZC_SR_SETT, 16#00#);
+      --  Disable the digital soft ramp
+      This.I2C_Write (CS43L22_REG_MISC_CTL, 16#04#);
+      --  Disable the limiter attack level
+      This.I2C_Write (CS43L22_REG_LIMIT_CTL1, 16#00#);
+      --  Adjust Bass and Treble levels */
+      This.I2C_Write (CS43L22_REG_TONE_CTL, 16#0F#);
+      --  Adjust PCM volume level */
+      This.I2C_Write (CS43L22_REG_PCMA_VOL, 16#0A#);
+      This.I2C_Write (CS43L22_REG_PCMB_VOL, 16#0A#);
+   end Init;
+
+   -------------
+   -- Read_ID --
+   -------------
+
+   function Read_ID (This : in out CS43L22_Device) return Unsigned_8 is
+   begin
+      return This.I2C_Read (CS43L22_REG_ID) and CS43L22_ID_MASK;
+   end Read_ID;
+
+   ----------
+   -- Play --
+   ----------
+
+   procedure Play (This : in out CS43L22_Device) is
+   begin
+      if not This.Output_Enabled then
+         --  Enable the digital soft ramp
+         This.I2C_Write (CS43L22_REG_MISC_CTL, 16#06#);
+
+         --  Enable output device
+         This.Set_Mute (Mute_Off);
+
+         --  Power on the Codec
+         This.I2C_Write (CS43L22_REG_POWER_CTL1, 16#9E#);
+
+         This.Output_Enabled := True;
+      end if;
+   end Play;
+
+   -----------
+   -- Pause --
+   -----------
+
+   procedure Pause (This : in out CS43L22_Device) is
+   begin
+      --  Pause the audio playing
+      This.Set_Mute (Mute_On);
+
+      --  CODEC in powersave mode
+      This.I2C_Write (CS43L22_REG_POWER_CTL1, 16#01#);
+   end Pause;
+
+   ------------
+   -- Resume --
+   ------------
+
+   procedure Resume (This : in out CS43L22_Device) is
+   begin
+      --  Unmute the output first
+      This.Set_Mute (Mute_Off);
+
+      This.I2C_Write (CS43L22_REG_POWER_CTL2, This.Output_Dev);
+
+      --  Exit the power save mode
+      This.I2C_Write (CS43L22_REG_POWER_CTL1, 16#9E#);
+   end Resume;
+
+   ----------
+   -- Stop --
+   ----------
+
+   procedure Stop (This : in out CS43L22_Device) is
+   begin
+      if This.Output_Enabled then
+         --  Mute the output first
+         This.Set_Mute (Mute_On);
+
+         --  Disable the digital soft ramp
+         This.I2C_Write (CS43L22_REG_MISC_CTL, 16#04#);
+
+         --  Power down the DAC and the speaker
+         This.I2C_Write (CS43L22_REG_POWER_CTL1, 16#9F#);
+
+         This.Output_Enabled := False;
+      end if;
+   end Stop;
+
+   ----------------
+   -- Set_Volume --
+   ----------------
+
+   procedure Set_Volume (This : in out CS43L22_Device; Volume : Volume_Level)
+   is
+      --  Actual Volume in range 0 .. 16#3F#
+      Converted_Volume : Unsigned_8 :=
+                           Unsigned_8 (Unsigned_16 (Volume) * 200 / 100);
+
+   begin
+      --  range goes the following:
+      --  0 dB .. +12 dB:     coded from 0 to 24
+      --  -102 dB .. -0.5 dB: coded from 52 to 255
+      --  -102 dB:            applied for values in range 25 .. 52
+      --  so we have a valid range of volume from -102 to +12 in steps of 0.5
+      --  which means 225 significant values
+      --  200 .. 224 for positive dB
+      --  0 .. 199 for negative dB
+
+      --  However positive values may lead to saturated output. We thus limit
+      --  the volume to the -102 .. 0dB range
+
+      if Converted_Volume >= 200 then
+         Converted_Volume := Converted_Volume - 200;
+      else
+         Converted_Volume := Converted_Volume + 52;
+      end if;
+
+      This.I2C_Write (CS43L22_REG_MASTER_A_VOL, Converted_Volume);
+      This.I2C_Write (CS43L22_REG_MASTER_B_VOL, Converted_Volume);
+   end Set_Volume;
+
+   --------------
+   -- Set_Mute --
+   --------------
+
+   procedure Set_Mute (This : in out CS43L22_Device; Cmd : Mute) is
+   begin
+      if This.Output_Enabled then
+         case Cmd is
+            when Mute_On =>
+               This.I2C_Write (CS43L22_REG_POWER_CTL2, 16#FF#);
+               This.I2C_Write (CS43L22_REG_HEADPHONE_A_VOL, 16#01#);
+               This.I2C_Write (CS43L22_REG_HEADPHONE_B_VOL, 16#01#);
+            when Mute_Off =>
+               This.I2C_Write (CS43L22_REG_HEADPHONE_A_VOL, 16#00#);
+               This.I2C_Write (CS43L22_REG_HEADPHONE_B_VOL, 16#00#);
+               This.I2C_Write (CS43L22_REG_POWER_CTL2, This.Output_Dev);
+         end case;
+      end if;
+   end Set_Mute;
+
+   ---------------------
+   -- Set_Output_Mode --
+   ---------------------
+
+   procedure Set_Output_Mode (This   : in out CS43L22_Device;
+                              Device : Output_Device)
+   is
+   begin
+      case Device is
+         when No_Output =>
+            This.Output_Dev := 0;
+         when Speaker =>
+            This.Output_Dev := 16#FA#;
+         when Headphone =>
+            This.Output_Dev := 16#AF#;
+         when Both =>
+            This.Output_Dev := 16#AA#;
+         when Auto =>
+            This.Output_Dev := 16#05#;
+      end case;
+
+      This.I2C_Write (CS43L22_REG_POWER_CTL2, This.Output_Dev);
+   end Set_Output_Mode;
+
+   -------------------
+   -- Set_Frequency --
+   -------------------
+
+   procedure Set_Frequency (This  : in out CS43L22_Device;
+                            Freq : Audio_Frequency)
+   is
+      pragma Unreferenced (This, Freq);
+   begin
+      --  CODEC is automatically detecting the frequency. No need to do
+      --  anything here.
+      null;
+   end Set_Frequency;
+
+   -----------
+   -- Reset --
+   -----------
+
+   procedure Reset (This : in out CS43L22_Device)
+   is
+      pragma Unreferenced (This);
+   begin
+      null;
+   end Reset;
+
+end CS43L22;
---
+++ components/src/audio/CS43L22/cs43l22.ads
@@ -0,0 +1,123 @@
+--  Driver for the WM8994 CODEC
+
+with Interfaces; use Interfaces;
+with HAL;        use HAL;
+with HAL.I2C;    use HAL.I2C;
+
+package CS43L22 is
+
+   type Output_Device is
+     (No_Output,
+      Speaker,
+      Headphone,
+      Both,
+      Auto);
+
+   CS43L22_I2C_Addr : constant := 16#94#;
+
+   CS43L22_ID       : constant := 16#E0#;
+   CS43L22_ID_MASK  : constant := 16#F8#;
+
+   type Audio_Frequency is
+     (Audio_Freq_8kHz,
+      Audio_Freq_11kHz,
+      Audio_Freq_16kHz,
+      Audio_Freq_22kHz,
+      Audio_Freq_32kHz,
+      Audio_Freq_44kHz,
+      Audio_Freq_48kHz,
+      Audio_Freq_96kHz,
+      Audio_Freq_192kHz)
+     with Size => 32;
+   for Audio_Frequency use
+     (Audio_Freq_8kHz   =>  8_000,
+      Audio_Freq_11kHz  => 11_025,
+      Audio_Freq_16kHz  => 16_000,
+      Audio_Freq_22kHz  => 22_050,
+      Audio_Freq_32kHz  => 32_000,
+      Audio_Freq_44kHz  => 44_100,
+      Audio_Freq_48kHz  => 48_000,
+      Audio_Freq_96kHz  => 96_000,
+      Audio_Freq_192kHz => 192_000);
+
+   type Mute is
+     (Mute_On,
+      Mute_Off);
+
+   subtype Volume_Level is Unsigned_8 range 0 .. 100;
+
+   type CS43L22_Device (Port : not null I2C_Port_Ref) is
+     tagged limited private;
+
+   procedure Init (This      : in out CS43L22_Device;
+                   Output    : Output_Device;
+                   Volume    : Volume_Level;
+                   Frequency : Audio_Frequency);
+
+   function Read_ID (This : in out CS43L22_Device) return Unsigned_8;
+   procedure Play (This : in out CS43L22_Device);
+   procedure Pause (This : in out CS43L22_Device);
+   procedure Resume (This : in out CS43L22_Device);
+   procedure Stop (This : in out CS43L22_Device);
+   procedure Set_Volume (This : in out CS43L22_Device; Volume : Volume_Level);
+   procedure Set_Mute (This : in out CS43L22_Device; Cmd : Mute);
+   procedure Set_Output_Mode (This : in out CS43L22_Device;
+                              Device : Output_Device);
+   procedure Set_Frequency (This : in out CS43L22_Device;
+                            Freq : Audio_Frequency);
+   procedure Reset (This : in out CS43L22_Device);
+
+private
+
+   CS43L22_REG_ID                  : constant := 16#01#;
+   CS43L22_REG_POWER_CTL1          : constant := 16#02#;
+   CS43L22_REG_POWER_CTL2          : constant := 16#04#;
+   CS43L22_REG_CLOCKING_CTL        : constant := 16#05#;
+   CS43L22_REG_INTERFACE_CTL1      : constant := 16#06#;
+   CS43L22_REG_INTERFACE_CTL2      : constant := 16#07#;
+   CS43L22_REG_PASSTHR_A_SELECT    : constant := 16#08#;
+   CS43L22_REG_PASSTHR_B_SELECT    : constant := 16#09#;
+   CS43L22_REG_ANALOG_ZC_SR_SETT   : constant := 16#0A#;
+   CS43L22_REG_PASSTHR_GANG_CTL    : constant := 16#0C#;
+   CS43L22_REG_PLAYBACK_CTL1       : constant := 16#0D#;
+   CS43L22_REG_MISC_CTL            : constant := 16#0E#;
+   CS43L22_REG_PLAYBACK_CTL2       : constant := 16#0F#;
+   CS43L22_REG_PASSTHR_A_VOL       : constant := 16#14#;
+   CS43L22_REG_PASSTHR_B_VOL       : constant := 16#15#;
+   CS43L22_REG_PCMA_VOL            : constant := 16#1A#;
+   CS43L22_REG_PCMB_VOL            : constant := 16#1B#;
+   CS43L22_REG_BEEP_FREQ_ON_TIME   : constant := 16#1C#;
+   CS43L22_REG_BEEP_VOL_OFF_TIME   : constant := 16#1D#;
+   CS43L22_REG_BEEP_TONE_CFG       : constant := 16#1E#;
+   CS43L22_REG_TONE_CTL            : constant := 16#1F#;
+   CS43L22_REG_MASTER_A_VOL        : constant := 16#20#;
+   CS43L22_REG_MASTER_B_VOL        : constant := 16#21#;
+   CS43L22_REG_HEADPHONE_A_VOL     : constant := 16#22#;
+   CS43L22_REG_HEADPHONE_B_VOL     : constant := 16#23#;
+   CS43L22_REG_SPEAKER_A_VOL       : constant := 16#24#;
+   CS43L22_REG_SPEAKER_B_VOL       : constant := 16#25#;
+   CS43L22_REG_CH_MIXER_SWAP       : constant := 16#26#;
+   CS43L22_REG_LIMIT_CTL1          : constant := 16#27#;
+   CS43L22_REG_LIMIT_CTL2          : constant := 16#28#;
+   CS43L22_REG_LIMIT_ATTACK_RATE   : constant := 16#29#;
+   CS43L22_REG_OVF_CLK_STATUS      : constant := 16#2E#;
+   CS43L22_REG_BATT_COMPENSATION   : constant := 16#2F#;
+   CS43L22_REG_VP_BATTERY_LEVEL    : constant := 16#30#;
+   CS43L22_REG_SPEAKER_STATUS      : constant := 16#31#;
+   CS43L22_REG_TEMPMONITOR_CTL     : constant := 16#32#;
+   CS43L22_REG_THERMAL_FOLDBACK    : constant := 16#33#;
+   CS43L22_REG_CHARGE_PUMP_FREQ    : constant := 16#34#;
+
+   type CS43L22_Device (Port : not null I2C_Port_Ref) is tagged limited record
+      Output_Enabled : Boolean := False;
+      Output_Dev     : Byte := 0;
+   end record;
+
+   procedure I2C_Write (This  : in out CS43L22_Device;
+                        Reg   : Byte;
+                        Value : Byte);
+   function I2C_Read (This : in out CS43L22_Device;
+                      Reg  : Byte)
+                      return Byte;
+
+end CS43L22;
