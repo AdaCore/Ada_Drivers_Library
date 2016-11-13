fd879c954cd14067973a5099bd0abd8da9fbf8bf
Fabien Chouteau <chouteau@adacore.com>
2016-11-13 19:19:17 +0100

    OpenMV: Add OV7725 sensor support
    The "post kickstarter" OpenMV2 modules are using the OV7725 module.

--- boards/OpenMV2/src/openmv-sensor.adb
+++ boards/OpenMV2/src/openmv-sensor.adb
@@ -2,6 +2,7 @@ with STM32.DCMI;
 with STM32.DMA;     use STM32.DMA;
 with Ada.Real_Time; use Ada.Real_Time;
 with OV2640;        use OV2640;
+with OV7725;        use OV7725;
 with Interfaces;    use Interfaces;
 with HAL.I2C;       use HAL.I2C;
 with HAL.Bitmap;    use HAL.Bitmap;
@@ -15,8 +16,10 @@ package body OpenMV.Sensor is
    REG_PID : constant := 16#0A#;
    --  REG_VER : constant := 16#0B#;
 
-   CLK_PWM_Mod : PWM_Modulator;
-   Camera      : OV2640_Cam (Sensor_I2C'Access);
+   CLK_PWM_Mod    : PWM_Modulator;
+   Camera_PID     : HAL.Byte := 0;
+   Camera_2640    : OV2640_Cam (Sensor_I2C'Access);
+   Camera_7725    : OV7725_Cam (Sensor_I2C'Access);
    Is_Initialized : Boolean := False;
 
    -----------------
@@ -41,6 +44,7 @@ package body OpenMV.Sensor is
             Cam_Addr := Addr;
             return True;
          end if;
+         delay until Clock + Milliseconds (1);
       end loop;
       return False;
    end Probe;
@@ -84,7 +88,6 @@ package body OpenMV.Sensor is
          Cam_Addr : UInt10;
          Data : I2C_Data (1 .. 1);
          Status : I2C_Status;
-         PID : HAL.Byte;
       begin
 
          --  Power cycle
@@ -103,8 +106,6 @@ package body OpenMV.Sensor is
          if  not Probe (Cam_Addr) then
 
             --  Retry with reversed reset polarity
-            Clear (DCMI_RST);
-            delay until Clock + Milliseconds (10);
             Set (DCMI_RST);
             delay until Clock + Milliseconds (10);
 
@@ -112,6 +113,8 @@ package body OpenMV.Sensor is
                raise Program_Error;
             end if;
          end if;
+
+         delay until Clock + Milliseconds (10);
 
          --  Select sensor bank
          Sensor_I2C.Mem_Write (Addr          => Cam_Addr,
@@ -123,23 +126,40 @@ package body OpenMV.Sensor is
             raise Program_Error;
          end if;
 
-         Sensor_I2C.Mem_Read (Addr          => Cam_Addr,
-                              Mem_Addr      => REG_PID,
-                              Mem_Addr_Size => Memory_Size_8b,
-                              Data          => Data,
-                              Status        => Status);
+         delay until Clock + Milliseconds (10);
+
+         Sensor_I2C.Master_Transmit (Addr    => Cam_Addr,
+                                     Data    => (1 => REG_PID),
+                                     Status  => Status);
          if Status /= Ok then
             raise Program_Error;
          end if;
-         PID := Data (Data'First);
 
-         if PID /= OV2640_PID then
+         Sensor_I2C.Master_Receive (Addr    => Cam_Addr,
+                                    Data    => Data,
+                                    Status  => Status);
+         if Status /= Ok then
             raise Program_Error;
          end if;
 
-         Initialize (Camera, Cam_Addr);
-         Set_Pixel_Format (Camera, Pix_RGB565);
-         Set_Frame_Size (Camera, QQVGA2);
+         if Status /= Ok then
+            raise Program_Error;
+         end if;
+         Camera_PID := Data (Data'First);
+
+         case Camera_PID is
+            when OV2640_PID =>
+               Initialize (Camera_2640, Cam_Addr);
+               Set_Pixel_Format (Camera_2640, Pix_RGB565);
+               Set_Frame_Size (Camera_2640, QQVGA2);
+            when OV7725_PID =>
+               Initialize (Camera_7725, Cam_Addr);
+               Set_Pixel_Format (Camera_7725, Pix_RGB565);
+               Set_Frame_Size (Camera_7725, QQVGA2);
+            when others =>
+               raise Program_Error with "Unknown camera module";
+         end case;
+
       end Initialize_Camera;
 
       -------------------
@@ -178,7 +198,7 @@ package body OpenMV.Sensor is
              Own_Address              => 16#00#,
              Addressing_Mode          => Addressing_Mode_7bit,
              General_Call_Enabled     => False,
-             Clock_Stretching_Enabled => True,
+             Clock_Stretching_Enabled => False,
              Clock_Speed              => 10_000));
 
          Enable_Clock (DCMI_Out_Points);
@@ -202,14 +222,29 @@ package body OpenMV.Sensor is
       ---------------------
 
       procedure Initialize_DCMI is
+         Vertical    : DCMI.DCMI_Polarity;
+         Horizontal  : DCMI.DCMI_Polarity;
+         Pixel_Clock : DCMI.DCMI_Polarity;
       begin
+         case Camera_PID is
+            when OV2640_PID =>
+               Vertical    := DCMI.Active_Low;
+               Horizontal  := DCMI.Active_Low;
+               Pixel_Clock := DCMI.Active_High;
+            when OV7725_PID =>
+               Vertical    := DCMI.Active_High;
+               Horizontal  := DCMI.Active_Low;
+               Pixel_Clock := DCMI.Active_High;
+            when others =>
+               raise Program_Error with "Unknown camera module";
+         end case;
+
          Enable_DCMI_Clock;
          DCMI.Configure (Data_Mode            => DCMI.DCMI_8bit,
                          Capture_Rate         => DCMI.Capture_All,
-                         --  Sensor specific (OV2640)
-                         Vertical_Polarity    => DCMI.Active_Low,
-                         Horizontal_Polarity  => DCMI.Active_Low,
-                         Pixel_Clock_Polarity => DCMI.Active_High,
+                         Vertical_Polarity    => Vertical,
+                         Horizontal_Polarity  => Horizontal,
+                         Pixel_Clock_Polarity => Pixel_Clock,
 
                          Hardware_Sync        => True,
                          JPEG                 => False);
@@ -253,6 +288,8 @@ package body OpenMV.Sensor is
 
    procedure Snapshot (BM : HAL.Bitmap.Bitmap_Buffer'Class) is
       Status : DMA_Error_Code;
+      Cnt : constant Interfaces.Unsigned_16 :=
+        Interfaces.Unsigned_16 ((BM.Width * BM.Height) / 2);
    begin
       if BM.Width /= Image_Width or else BM.Height /= Image_Height then
          raise Program_Error;
@@ -266,24 +303,29 @@ package body OpenMV.Sensor is
          raise Program_Error;
       end if;
 
+      Clear_All_Status (Sensor_DMA, Sensor_DMA_Stream);
+
       Start_Transfer (This        => Sensor_DMA,
                       Stream      => Sensor_DMA_Stream,
                       Source      => DCMI.Data_Register_Address,
                       Destination => BM.Addr,
-                      Data_Count  =>
-                        Interfaces.Unsigned_16 ((BM.Width * BM.Height) / 2));
+                      Data_Count  => Cnt);
 
       DCMI.Start_Capture (DCMI.Snapshot);
 
       Poll_For_Completion (Sensor_DMA,
                            Sensor_DMA_Stream,
                            Full_Transfer,
-                           Milliseconds (1000),
+                           Milliseconds (100),
                            Status);
+
       if Status /= DMA_No_Error then
-         Abort_Transfer (Sensor_DMA, Sensor_DMA_Stream, Status);
-         pragma Unreferenced (Status);
-         raise Program_Error;
+         if Status = DMA_Timeout_Error then
+            raise Program_Error with "DMA timeout! Transferred: " &
+              Items_Transferred (Sensor_DMA, Sensor_DMA_Stream)'Img;
+         else
+            raise Program_Error;
+         end if;
       end if;
    end Snapshot;
 
