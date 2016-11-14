bb4f265c47e6f04796bb45d8e872e4eb62244246
Fabien Chouteau <chouteau@adacore.com>
2016-11-14 12:52:16 +0100

    Components: Change OV camera type name

--- components/src/camera/OV2640/ov2640.adb
+++ components/src/camera/OV2640/ov2640.adb
@@ -243,17 +243,17 @@ package body OV2640 is
       (16#00#,     16#00#)
      );
 
-   procedure Write (This : OV2640_Cam; Addr, Data : Byte);
-   function Read (This : OV2640_Cam; Addr : Byte) return Byte;
-   procedure Select_Sensor_Bank (This : OV2640_Cam);
-   procedure Select_DSP_Bank (This : OV2640_Cam);
-   procedure Enable_DSP (This : OV2640_Cam; Enable : Boolean);
+   procedure Write (This : OV2640_Camera; Addr, Data : Byte);
+   function Read (This : OV2640_Camera; Addr : Byte) return Byte;
+   procedure Select_Sensor_Bank (This : OV2640_Camera);
+   procedure Select_DSP_Bank (This : OV2640_Camera);
+   procedure Enable_DSP (This : OV2640_Camera; Enable : Boolean);
 
    -----------
    -- Write --
    -----------
 
-   procedure Write (This : OV2640_Cam; Addr, Data : Byte) is
+   procedure Write (This : OV2640_Camera; Addr, Data : Byte) is
       Status : I2C_Status;
    begin
       This.I2C.Mem_Write (Addr          => This.Addr,
@@ -270,7 +270,7 @@ package body OV2640 is
    -- Read --
    ----------
 
-   function Read (This : OV2640_Cam; Addr : Byte) return Byte is
+   function Read (This : OV2640_Camera; Addr : Byte) return Byte is
       Data : I2C_Data (1 .. 1);
       Status : I2C_Status;
    begin
@@ -289,7 +289,7 @@ package body OV2640 is
    -- Select_Sensor_Bank --
    ------------------------
 
-   procedure Select_Sensor_Bank (This : OV2640_Cam) is
+   procedure Select_Sensor_Bank (This : OV2640_Camera) is
    begin
       Write (This, REG_BANK_SELECT, 1);
    end Select_Sensor_Bank;
@@ -298,7 +298,7 @@ package body OV2640 is
    -- Select_DSP_Bank --
    ---------------------
 
-   procedure Select_DSP_Bank (This : OV2640_Cam) is
+   procedure Select_DSP_Bank (This : OV2640_Camera) is
    begin
       Write (This, REG_BANK_SELECT, 0);
    end Select_DSP_Bank;
@@ -307,7 +307,7 @@ package body OV2640 is
    -- Enable_DSP --
    ----------------
 
-   procedure Enable_DSP (This : OV2640_Cam; Enable : Boolean) is
+   procedure Enable_DSP (This : OV2640_Camera; Enable : Boolean) is
    begin
       Select_DSP_Bank (This);
       Write (This, REG_DSP_BYPASS, (if Enable then 0 else 1));
@@ -318,7 +318,7 @@ package body OV2640 is
    ----------------
 
    procedure Initialize
-     (This : in out OV2640_Cam;
+     (This : in out OV2640_Camera;
       Addr : UInt10)
    is
    begin
@@ -334,7 +334,7 @@ package body OV2640 is
    ----------------------
 
    procedure Set_Pixel_Format
-     (This : OV2640_Cam;
+     (This : OV2640_Camera;
       Pix : Pixel_Format)
    is
    begin
@@ -359,7 +359,7 @@ package body OV2640 is
    --------------------
 
    procedure Set_Frame_Size
-     (This : OV2640_Cam;
+     (This : OV2640_Camera;
       Res  : Frame_Size)
    is
       H_SIZE, V_SIZE : Bit_Field (0 .. 15);
@@ -474,7 +474,7 @@ package body OV2640 is
    --------------------
 
    procedure Set_Frame_Rate
-     (This : OV2640_Cam;
+     (This : OV2640_Camera;
       FR  : Frame_Rate)
    is
    begin
@@ -485,7 +485,7 @@ package body OV2640 is
    -- Get_PID --
    -------------
 
-   function Get_PID (This : OV2640_Cam) return Byte is
+   function Get_PID (This : OV2640_Camera) return Byte is
    begin
       Select_Sensor_Bank (This);
       return Read (This, REG_SENSOR_PID);
@@ -495,7 +495,7 @@ package body OV2640 is
    -- Enable_Auto_Gain_Control --
    ------------------------------
 
-   procedure Enable_Auto_Gain_Control (This   : OV2640_Cam;
+   procedure Enable_Auto_Gain_Control (This   : OV2640_Camera;
                                        Enable : Boolean := True)
    is
       COM8 : Byte;
@@ -515,7 +515,7 @@ package body OV2640 is
    -- Enable_Auto_White_Balance --
    -------------------------------
 
-   procedure Enable_Auto_White_Balance (This   : OV2640_Cam;
+   procedure Enable_Auto_White_Balance (This   : OV2640_Camera;
                                         Enable : Boolean := True)
    is
       CTRL1 : Byte;
@@ -535,7 +535,7 @@ package body OV2640 is
    -- Enable_Auto_Exposure_Control --
    ----------------------------------
 
-   procedure Enable_Auto_Exposure_Control (This   : OV2640_Cam;
+   procedure Enable_Auto_Exposure_Control (This   : OV2640_Camera;
                                            Enable : Boolean := True)
    is
       CTRL0 : Byte;
@@ -555,7 +555,7 @@ package body OV2640 is
    -- Enable_Auto_Band_Filter --
    -----------------------------
 
-   procedure Enable_Auto_Band_Filter (This   : OV2640_Cam;
+   procedure Enable_Auto_Band_Filter (This   : OV2640_Camera;
                                       Enable : Boolean := True)
    is
       COM8 : Byte;
