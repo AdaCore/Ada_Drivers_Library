8d9ea06b82673442cedf0be0052b8258ea81eaf8
Tristan Gingold <gingold@adacore.com>
2016-11-18 10:48:25 +0100

    hal-block_drivers: define block_size, add pre-conditions.

--- hal/src/hal-block_drivers.ads
+++ hal/src/hal-block_drivers.ads
@@ -38,16 +38,21 @@ package HAL.Block_Drivers is
 
    subtype Block is Byte_Array;
 
+   Block_Size : constant := 512;
+   --  Size of a block, for block number.
+
    function Read
      (This         : in out Block_Driver;
       Block_Number : Unsigned_64;
       Data         : out Block)
-      return Boolean is abstract;
+     return Boolean is abstract
+     with Pre'Class => Data'Length mod Block_Size = 0;
 
    function Write
      (This         : in out Block_Driver;
       Block_Number : Unsigned_64;
       Data         : Block)
-      return Boolean is abstract;
+     return Boolean is abstract
+     with Pre'Class => Data'Length mod Block_Size = 0;
 
 end HAL.Block_Drivers;
