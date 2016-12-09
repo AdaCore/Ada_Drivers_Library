5a722ddc03e068e3940931e18646329734148eaf
Fabien Chouteau <chouteau@adacore.com>
2016-12-09 19:25:53 +0100

    STM32.DMA.Interrupts: Create a DMA transfer service with interrupts

---
+++ arch/ARM/STM32/drivers/dma/stm32-dma-interrupts.ads
@@ -0,0 +1,61 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                       Copyright (C) 2016, AdaCore                        --
+--                                                                          --
+--  Redistribution and use in source and binary forms, with or without      --
+--  modification, are permitted provided that the following conditions are  --
+--  met:                                                                    --
+--     1. Redistributions of source code must retain the above copyright    --
+--        notice, this list of conditions and the following disclaimer.     --
+--     2. Redistributions in binary form must reproduce the above copyright --
+--        notice, this list of conditions and the following disclaimer in   --
+--        the documentation and/or other materials provided with the        --
+--        distribution.                                                     --
+--     3. Neither the name of the copyright holder nor the names of its     --
+--        contributors may be used to endorse or promote products derived   --
+--        from this software without specific prior written permission.     --
+--                                                                          --
+--   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    --
+--   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      --
+--   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR  --
+--   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT   --
+--   HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, --
+--   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       --
+--   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  --
+--   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  --
+--   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT    --
+--   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  --
+--   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with Ada.Interrupts;
+
+package STM32.DMA.Interrupts is
+
+   protected type DMA_Interrupt_Controller
+     (Controller : not null access DMA_Controller;
+      Stream     : DMA_Stream_Selector;
+      ID         : Ada.Interrupts.Interrupt_ID)
+   is
+
+      procedure Start_Transfer (Source      : Address;
+                                Destination : Address;
+                                Data_Count  : UInt16);
+
+      procedure Abort_Transfer (Result : out DMA_Error_Code);
+
+      entry Wait_For_Completion (Status : out DMA_Error_Code);
+
+   private
+
+      procedure Interrupt_Handler;
+      pragma Attach_Handler (Interrupt_Handler, ID);
+
+      No_Transfer_In_Progess : Boolean := True;
+      Last_Status            : DMA_Error_Code := DMA_No_Error;
+   end DMA_Interrupt_Controller;
+
+   type DMA_Interrupt_Controller_Ref is access all DMA_Interrupt_Controller;
+
+end STM32.DMA.Interrupts;
