17854861424121503c4d441435d4e626fe1ef4da
Fabien Chouteau <fabien.chouteau@gmail.com>
2017-02-12 00:16:07 +0100

    HiFive1: Add pin definitions

--- boards/HiFive1/src/hifive1.ads
+++ boards/HiFive1/src/hifive1.ads
@@ -29,5 +29,30 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
+with FE310.Device; use FE310.Device;
+with FE310.GPIO;   use FE310.GPIO;
+
 package HiFive1 is
+
+   HF1_Pin_0  : GPIO_Point renames P16;
+   HF1_Pin_1  : GPIO_Point renames P17;
+   HF1_Pin_2  : GPIO_Point renames P18;
+   HF1_Pin_3  : GPIO_Point renames P19; --  Green LED
+   HF1_Pin_4  : GPIO_Point renames P20;
+   HF1_Pin_5  : GPIO_Point renames P21; --  Blue LED
+   HF1_Pin_6  : GPIO_Point renames P22; --  Red LED
+   HF1_Pin_7  : GPIO_Point renames P23;
+   HF1_Pin_8  : GPIO_Point renames P00;
+   HF1_Pin_9  : GPIO_Point renames P01;
+   HF1_Pin_10 : GPIO_Point renames P02;
+   HF1_Pin_11 : GPIO_Point renames P03;
+   HF1_Pin_12 : GPIO_Point renames P04;
+   HF1_Pin_13 : GPIO_Point renames P05;
+   --  HF1_Pin_14 is not connected
+   HF1_Pin_15 : GPIO_Point renames P09;
+   HF1_Pin_16 : GPIO_Point renames P10;
+   HF1_Pin_17 : GPIO_Point renames P11;
+   HF1_Pin_18 : GPIO_Point renames P12;
+   HF1_Pin_19 : GPIO_Point renames P13;
+
 end HiFive1;
