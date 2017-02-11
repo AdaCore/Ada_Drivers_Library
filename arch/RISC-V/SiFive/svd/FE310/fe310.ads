179a690886d45b63057d2d44dcf64843a2479b15
Fabien Chouteau <fabien.chouteau@gmail.com>
2017-02-11 15:29:37 +0100

    RISC-V: Start Freedom E310 support

---
+++ arch/RISC-V/SiFive/svd/FE310/fe310.ads
@@ -0,0 +1,23 @@
+--  
+
+--  This spec has been automatically generated from FE310.svd
+
+pragma Restrictions (No_Elaboration_Code);
+pragma Ada_2012;
+pragma Style_Checks (Off);
+
+with System;
+
+--  E31 CPU Coreplex, high-performance, 32-bit RV32IMAC core
+--    
+package FE310 is
+   pragma Preelaborate;
+
+   --------------------
+   -- Base addresses --
+   --------------------
+
+   GPIO_Base : constant System.Address :=
+     System'To_Address (16#10012000#);
+
+end FE310;
