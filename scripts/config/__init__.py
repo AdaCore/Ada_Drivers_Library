56bc9faa63415cea68b25db22d67c4deffcad98b
Fabien Chouteau <chouteau@adacore.com>
2018-04-24 17:47:01 +0200

    Create project configuration scripts

---
+++ scripts/config/__init__.py
@@ -0,0 +1,305 @@
+#! /usr/bin/env python
+
+import sys
+from validation import Int_Validation, Float_Validation, \
+     Enum_Validation, String_Validation, Bool_Validation
+
+
+class Database:
+
+    def __init__(self, interactive, use_default, user_input):
+        self.pre_defined_params = {}
+        self.configuration = {}
+        self.memories = []
+        self.src_dirs = []
+        self.interactive = interactive
+        self.use_default = use_default
+        self.user_input = user_input
+
+    def pre_define(self, key, value, origin):
+        self.pre_defined_params[key] = {'value': value,
+                                        'origin': origin}
+
+    def print_remaining_pre_defined(self):
+        for key in self.pre_defined_params:
+            origin = self.pre_defined_params[key]['origin']
+            print "warning: key '%s' from %s " + \
+                  "was not used in the configuration" \
+                  % (key, origin)
+
+    def add_to_configuration(self, key, kind, value, origin):
+        self.configuration[key] = {'value':  value,
+                                   'kind':   kind,
+                                   'origin': origin}
+
+    def get_config(self, key):
+        if key in self.configuration:
+            return self.configuration[key]['value']
+        else:
+            return None
+
+    def ada_configuration(self):
+        out = ""
+        for key in self.configuration:
+            origin = self.configuration[key]['origin']
+            value = self.configuration[key]['value']
+            kind = self.configuration[key]['kind']
+            if kind == "int" or kind == "float":
+                out += "   %-30s : constant         := %-20s -- From %s\n" % \
+                      (key, str(value) + ";", origin)
+            elif kind == "enum" or kind == "string":
+                out += "   %-30s : constant String  := %-20s -- From %s\n" % \
+                      (key, "\"" + str(value) + "\";", origin)
+            elif kind == "bool":
+                out += "   %-30s : constant Boolean := %-20s -- From %s\n" % \
+                      (key, str(value), origin)
+            else:
+                print "fatal error, unknown kind '%s' for config key '%s'" % \
+                      (kind, key)
+                sys.exit(1)
+
+        return out
+
+    def gpr_configuration(self, src_root_dir, extra_source_dir):
+        out = ""
+        arch = self.get_config("ARCH")
+        runtime = self.get_config("Runtime_Name")
+
+        if arch is not None and arch != "Native":
+            if arch == "ARM":
+                target = "arm-eabi"
+            elif arch == "RISC-V":
+                target = "riscv32-elf"
+
+            out += "   for Target use \"%s\";\n" % target
+
+        if runtime is not None:
+            out += "   for Runtime (\"Ada\") use \"%s\";\n" % runtime
+
+        out += "\n"
+
+        for key in self.configuration:
+            origin = self.configuration[key]['origin']
+            value = self.configuration[key]['value']
+            out += "   %-30s := %-20s -- From %s\n" % \
+                   (key, '"' + str(value) + '";', origin)
+
+        out += "\n   --  Project source directories\n"
+        out += "   Src_Dirs_Root := \"%s\";\n" % src_root_dir
+        out += "   for Source_Dirs use (\n"
+        for src in self.src_dirs:
+            out += "    Src_Dirs_Root & \"/%s\", -- From %s\n" % \
+                   (src['value'], src['origin'])
+        out += "     \"%s/\");\n" % extra_source_dir
+
+        return out
+
+    def ask_the_user(self, key, validation, default):
+        origin = "user input"
+        kind = validation.kind()
+
+        if kind == "int":
+            value = self.user_input.query_int(key,
+                                              validation.min_value,
+                                              validation.max_value,
+                                              default)
+        elif kind == "enum":
+            value = self.user_input.query_choice(key,
+                                                 validation.list_of_values,
+                                                 default)
+        elif kind == "string":
+            value = self.user_input.query_string(key, default)
+        elif kind == "float":
+            value = self.user_input.query_float(key,
+                                                validation.min_value,
+                                                validation.max_value,
+                                                default)
+        elif kind == "bool":
+            value = self.user_input.query_bool(key, default)
+
+        self.add_to_configuration(key,
+                                  validation.kind(),
+                                  value,
+                                  origin)
+
+    def use_pre_defined_param(self, key, validation):
+        value = self.pre_defined_params[key]['value']
+        origin = self.pre_defined_params[key]['origin']
+
+        if validation(value):
+            self.add_to_configuration(key, validation.kind(), value, origin)
+            print "For key '%s', take value '%s' from %s" % \
+                  (key, value, origin)
+            del self.pre_defined_params[key]
+        else:
+            print "Invalid value '%s' for key '%s' from %s" % \
+                  (value, key, origin)
+            print "Valid values are : %s" % str(validation)
+            sys.exit(1)
+
+    def query_key(self, key, validation=None, default=None):
+        if key in self.pre_defined_params:
+            self.use_pre_defined_param(key, validation)
+        elif self.interactive:
+            self.ask_the_user(key, validation, default)
+        elif default is not None:
+            if self.use_default:
+                print "For key '%s', use default value '%s'" % (key, default)
+                self.add_to_configuration(key,
+                                          validation.kind(),
+                                          default,
+                                          "default value")
+            else:
+                print "fatal error, user doesn't want to use default " + \
+                      "value for key '%s'" % key
+                sys.exit(1)
+        else:
+            print "fatal error, user input required for key '%s'" % key
+            sys.exit(1)
+
+    def query_integer_key(self,
+                          key,
+                          min_value=None,
+                          max_value=None,
+                          default=None):
+        self.query_key(key,
+                       validation=Int_Validation(min_value, max_value),
+                       default=default)
+
+    def query_float_key(self,
+                        key,
+                        min_value=None,
+                        max_value=None,
+                        default=None):
+        self.query_key(key,
+                       validation=Float_Validation(min_value, max_value),
+                       default=default)
+
+    def query_enum_key(self, key, list_of_values, default=None):
+        self.query_key(key,
+                       validation=Enum_Validation(list_of_values),
+                       default=default)
+
+    def query_string_key(self, key, default=None):
+        self.query_key(key, validation=String_Validation(), default=default)
+
+    def query_bool_key(self, key, default=None):
+        self.query_key(key, validation=Bool_Validation(), default=default)
+
+    def set_config_string_key(self, key, value, origin):
+        self.add_to_configuration(key, 'string', value, origin)
+
+    def set_config_key_int(self, key, value, origin):
+        self.add_to_configuration(key, 'int', value, origin)
+
+    def parse_input_config(self, file):
+        print "loading configuration keys from: '%s'" % file.name
+        line_cnt = 0
+        for line in file:
+            line_cnt += 1
+            key, val = line.partition("=>")[::2]
+            key = key.strip()
+            val = val.strip()
+            if key and val:
+                print "Add pre definition '%s' => '%s' from file '%s'" % \
+                      (key, val, file.name)
+                self.pre_define(key, val, "%s:%d" % (file.name, line_cnt))
+            else:
+                print "invalid input configuration line at %s:%d" % \
+                      (file.name, line_cnt)
+
+    # Memory layout config #
+
+    def add_memory(self, kind, name, addr, size):
+        self.memories += [{'kind': kind,
+                           'name': name,
+                           'addr': addr,
+                           'size': size}]
+
+    def print_linker_script(self):
+
+        text_section = self.get_config("Linker_Text_Section")
+        rodata_section = self.get_config("Linker_RO_Data_Section")
+        data_section = self.get_config("Linker_Data_Section")
+        bss_section = self.get_config("Linker_BSS_Section")
+
+        print "MEMORY"
+        print "{"
+        for mem in self.memories:
+            if mem['kind'] == 'ROM':
+                print " %s (rxai!w) : ORIGIN = %s, LENGTH = %s" % \
+                      (mem['name'], mem['addr'], mem['size'])
+            elif mem['kind'] == 'RAM':
+                print " %s (wxa!ri) : ORIGIN = %s, LENGTH = %s" % \
+                      (mem['name'], mem['addr'], mem['size'])
+            else:
+                print "fatal error, unknown memory kind '%s'" % mem['kind']
+                sys.exit(1)
+        print "}"
+
+        print "\n.text:"
+        print "{"
+        print " *(.text.unlikely .text.unlikely.*)"
+        print " *(.text.startup .text.startup.*)"
+        print " *(.text .text.*)"
+        print " *(.gnu.linkonce.t.*)"
+        print "} > %s" % text_section
+        print "\n.rodata:"
+        print "{"
+        print " *(.rdata)"
+        print " *(.rodata .rodata.*)"
+        print " *(.gnu.linkonce.r.*)"
+        print "} > %s" % rodata_section
+
+        for mem in self.memories:
+            if mem['kind'] == 'RAM':
+                print "\n.%s_data:" % mem['name']
+                print "{"
+                print " *(.%s_data .%s_data.*)" % \
+                      (mem['name'], mem['name'])
+                print "} > %s" % mem['name']
+            elif mem['kind'] == 'ROM':
+                print "\n.%s_rodata:" % mem['name']
+                print "{"
+                print " *(.%s_rodata .%s_rodata.*)" % \
+                      (mem['name'], mem['name'])
+                print "} > %s" % mem['name']
+
+        print "\n.data:"
+        print "{"
+        print " *(.data .data.*)"
+        print " *(.gnu.linkonce.d.*)"
+        print "} > %s" % data_section
+        print "\n.sdata:"
+        print "{"
+        print " *(.sdata .sdata.*)"
+        print " *(.gnu.linkonce.s.*)"
+        print "} > %s" % data_section
+        print "\n.bss:"
+        print "{"
+        print " *(.bss .bss.*)"
+        print "} > %s" % bss_section
+
+    def memory_names(self):
+        list = []
+        for mem in self.memories:
+            list.append(mem['name'])
+        return list
+
+    def default_rom_area(self):
+        for mem in self.memories:
+            if mem['kind'] == 'ROM':
+                return mem['name']
+        return None
+
+    def default_ram_area(self):
+        for mem in self.memories:
+            if mem['kind'] == 'RAM':
+                return mem['name']
+        return None
+
+    # Source directories config #
+
+    def add_source_dir(self, relative_path, origin):
+        self.src_dirs.append({'value':  relative_path,
+                              'origin': origin})
---
+++ scripts/project_wizard.py
@@ -0,0 +1,338 @@
+#! /usr/bin/env python
+
+import os
+import argparse
+import config
+import config.user_input.console
+
+from config.boards import list_of_boards, load_board_config
+from config.devices import list_of_devices, list_of_vendors, \
+     list_of_families, load_device_config
+from config.archs import list_of_archs, load_cpu_config
+
+script_dir = os.path.dirname(__file__)
+ADL_root_dir = os.path.abspath(os.path.join(script_dir, ".."))
+cwd = os.getcwd()
+
+interactive = True
+use_default = True
+
+
+def ensure_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def board_config(config):
+    config.query_enum_key("Board", list_of_boards(), "Custom_Board")
+
+    if config.get_config("Board") != "Custom_Board":
+        load_board_config(config)
+
+
+def mcu_config(config):
+
+    config.query_enum_key("Architecture", list_of_archs(config))
+
+    if config.get_config("Architecture") != "Native":
+        config.query_enum_key("Vendor", list_of_vendors(config))
+        config.query_enum_key("Device_Family", list_of_families(config))
+        config.query_enum_key("Device_Name", list_of_devices(config))
+
+        device = config.get_config("Device_Name")
+
+        if device.startswith("STM32F4"):
+            core = "ARM Cortex-M4F"
+        elif device.startswith("STM32F7"):
+            core = "ARM Cortex-M7F"
+        elif device.startswith("nRF51"):
+            core = "ARM Cortex-M0"
+        elif device.startswith("FE3"):
+            core = "RISC-V32"
+
+        config.set_config_string_key("CPU_Core", core, "mcu definition")
+
+        family = config.get_config("Device_Family")
+
+        if family.startswith("STM32"):
+            config.query_integer_key("High_Speed_External_Clock",
+                                     1000000, 32000000)
+        elif family.startswith("FE310"):
+            config.query_integer_key("FLASH_SIZE")
+
+        load_cpu_config(config)
+        load_device_config(config)
+
+    # TDOD: Is the SVD mapping available?
+    # TODO: Does the user want to generate it?
+
+
+def linker_script_config(config):
+    config.query_bool_key('Generate_Linker_Script', default="True")
+
+    if config.get_config('Generate_Linker_Script') == "True":
+        config.query_bool_key('Has_Custom_Memory_Area_1', default="False")
+
+        cnt = 1
+        while config.get_config('Has_Custom_Memory_Area_%d' % cnt) == "True":
+            config.query_enum_key('Custom_Mem_%d_Kind' % cnt, ['ROM', 'RAM'])
+            config.query_integer_key('Custom_Mem_%d_Addr' % cnt, 0)
+            config.query_integer_key('Custom_Mem_%d_Size' % cnt, 0)
+            config.query_string_key('Custom_Mem_%d_Name' % cnt,
+                                    default="custom_mem_%d" % cnt)
+
+            config.add_memory(config.get_config('Custom_Mem_%d_Kind' % cnt),
+                              config.get_config('Custom_Mem_%d_Name' % cnt),
+                              config.get_config('Custom_Mem_%d_Addr' % cnt),
+                              config.get_config('Custom_Mem_%d_Size' % cnt))
+
+            cnt += 1
+            config.query_bool_key('Has_Custom_Memory_Area_%d' % cnt,
+                                  default="False")
+
+        config.query_bool_key('Custom_Memory_Layout', default='False')
+        if config.get_config('Custom_Memory_Layout') == "True":
+
+            config.query_enum_key('Linker_Text_Section', config.memory_names(),
+                                  default=config.default_rom_area())
+            config.query_enum_key('Linker_RO_Data_Section',
+                                  config.memory_names(),
+                                  default=config.default_rom_area())
+            config.query_enum_key('Linker_Data_Section', config.memory_names(),
+                                  default=config.default_ram_area())
+            config.query_enum_key('Linker_BSS_Section', config.memory_names(),
+                                  default=config.default_ram_area())
+
+        print "\nLinker script:"
+        config.print_linker_script()
+
+
+def middleware_config(config):
+    # config.query_integer_key('MAX_PATH', 0, default="1024")
+    # config.query_integer_key('MAX_FILE_DESC', 0, default="10")
+
+    origin = "middleware config"
+    config.add_source_dir('middleware/src/filesystem', origin)
+    config.add_source_dir('middleware/src/BLE', origin)
+    config.add_source_dir('middleware/src/utils', origin)
+    config.add_source_dir('middleware/src/audio', origin)
+    config.add_source_dir('middleware/src/monitor', origin)
+    config.add_source_dir('middleware/src/bitmap', origin)
+    config.add_source_dir('middleware/sdmmc', origin)
+
+    runtime_profile = config.get_config("Runtime_Profile")
+
+    if runtime_profile is None:
+        runtime_profile = ''
+
+    if runtime_profile.startswith("ravenscar"):
+        config.add_source_dir('middleware/src/ravenscar-common', origin)
+
+
+def components_config(config):
+    config.add_source_dir('components/src/**', "components config")
+
+
+def runtime_config(config):
+    config.query_string_key('Has_ZFP_Runtime', default="False")
+    config.query_string_key('Has_Ravenscar_SFP_Runtime', default="False")
+    config.query_string_key('Has_Ravenscar_Full_Runtime', default="False")
+
+    profiles = []
+    if config.get_config("Has_Ravenscar_Full_Runtime") == "True":
+        profiles.append("ravenscar-full")
+    if config.get_config("Has_Ravenscar_SFP_Runtime") == "True":
+        profiles.append("ravenscar-sfp")
+    if config.get_config("Has_ZFP_Runtime") == "True":
+        profiles.append("zfp")
+
+    if len(profiles) > 0:
+        config.query_enum_key('Runtime_Profile',  profiles,
+                              default=profiles[0])
+
+        config.query_string_key('Runtime_Name_Suffix')
+
+        suffix = config.get_config("Runtime_Name_Suffix")
+        profile = config.get_config("Runtime_Profile")
+        config.query_string_key('Runtime_Name', default=profile + "-" + suffix)
+
+
+def ADL_configuration(config, project_directory, project_name,
+                      source_dir_name, object_dir_name):
+
+    config.add_source_dir('hal/src/', "HAL config")
+
+    board_config(config)
+    mcu_config(config)
+    runtime_config(config)
+    # Linker script configuration is not available for the moment
+    # linker_script_config(config)
+    middleware_config(config)
+    components_config(config)
+
+    print "The configuration is now finished."
+    print "Let's generate some files:"
+
+    relative_ADL_root_path = os.path.relpath(ADL_root_dir, project_directory)
+    source_dir = os.path.join(project_directory, source_dir_name)
+    ada_config_path = os.path.join(source_dir, "adl_config.ads")
+    gpr_path = os.path.join(project_directory, "%s.gpr" % project_name.lower())
+
+    #  GPR file
+    gpr = "--  This project file was generated by the " + \
+          "Ada_Drivers_Library project wizard script\n"
+    gpr += "library project %s is\n" % project_name
+
+    gpr += """
+   type BUILD_TYPE is ("Debug", "Production");
+   Build : BUILD_Type := external ("ADL_BUILD", "Debug");
+
+   --  Target architecture
+   Target := Project'Target;
+
+   --  Callgraph info is not available on all architectures
+   Callgraph_Switch := ();
+   case Target is
+      when "riscv32-unknown-elf" =>
+         null;
+      when others =>
+         Callgraph_Switch := ("-fcallgraph-info=su");
+   end case;
+
+   package Compiler is
+      case Build is
+         when "Production" =>
+            for Default_Switches ("Ada") use
+              ("-g", "-O3", "-gnatp", "-gnatn");
+         when "Debug" =>
+            for Default_Switches ("Ada") use
+              ("-g", "-O0", "-gnata") & Callgraph_Switch;
+      end case;
+      for Default_Switches ("ada") use Compiler'Default_Switches ("Ada") &
+        ("-gnatwa", "-gnatwe", "-gnatQ", "-gnatw.X", "-gnaty", "-gnatyO",
+         "-gnatyM120", "-ffunction-sections", "-fdata-sections");
+   end Compiler;
+
+"""
+    gpr += "   for Languages use (\"Ada\");\n"
+    gpr += "   for Create_Missing_Dirs use \"True\";\n"
+    gpr += "   for Object_Dir use \"%s_\" & Build;\n" % object_dir_name
+    gpr += "   for Library_Dir use \"%s_lib_\" & Build;\n" % object_dir_name
+    gpr += "   for Library_Kind use \"static\";\n"
+    gpr += "   for Library_Name use \"ada_drivers_library\";\n"
+
+    gpr += config.gpr_configuration(relative_ADL_root_path, source_dir_name)
+    gpr += "end %s;\n" % project_name
+    print " -> Writing gprbuild project file."
+    with open(gpr_path, "w") as f:
+        f.write(gpr)
+
+    # Ada config package
+    print " -> Writing the Ada configuration file."
+
+    ensure_dir(source_dir)
+
+    ada = "--  This package was generated by the " + \
+          "Ada_Drivers_Library project wizard script\n"
+    ada += "package ADL_Config is\n"
+    ada += config.ada_configuration()
+    ada += "end ADL_Config;\n"
+    with open(ada_config_path, "w") as f:
+        f.write(ada)
+
+    config.print_remaining_pre_defined()
+
+    print "Your Ada Drivers Library project is now ready to use."
+
+
+description = \
+"""Welcome to the Ada Drivers Library (ADL) project wizard. This script will
+ask you some questions to define the ADL configuration of your project. It will
+then generate the different files required to used ADL based on this
+configuration."""
+
+parser = argparse.ArgumentParser(description=description)
+
+parser.add_argument('definitions', nargs='*',
+                    help="""Pre defined config key=value. Use this to give predefined answer to the
+                    configuration question. For instant Board=STM32F407_Discovery""")
+
+parser.add_argument('-i', '--input-config', type=argparse.FileType('r'),
+                    action='append',
+                    help="""Load pre defined config keys from a file. The file must containt couples of
+                    key=value, only one per line and nothing else.""")
+
+parser.add_argument('--script-mode', action="store_true",
+                    help="""Enable script mode. In script mode there is no input required from the user.
+                    Configuration values will be taken from pre-defined values
+                    (--input-config or command line definitions), or default
+                    value if any. The configuration will fail if there are now
+                    pre-defined or default value for a key. This mode can be
+                    used to automatically generated ADL project file from a
+                    known configuration.""")
+
+parser.add_argument('--dont-use-defaults', action="store_true",
+                    help="""When this switch is used in script mode, the configuration will not take into
+                    account default values. This means that the configuration
+                    will fail if a key doesn't have a pre-defined value from
+                    --input-config or command line definitions. This can be
+                    used to automatically detect discrepency when upgrading to
+                    a new version of ADL, for instance if a new config key was
+                    added.""")
+
+
+parser.add_argument("-d", "--project-dir",
+                    help="""Directory where the project will be created (default is Ada_Drivers_Library).
+                    Use '.' to create the project in the current directory.""",
+                    default="Ada_Drivers_Library")
+
+parser.add_argument("-p", "--project-name",
+                    help="""Name of the project file that will be generated (default is
+                    Ada_Drivers_Library)""",
+                    default="Ada_Drivers_Library")
+
+parser.add_argument("-s", "--source-dir",
+                    help="""Name of the source directory for you ADL project (default is src). This is
+                    where the Ada confiuration file will be written. You can
+                    also use this directory to add you own package
+                    specification and/or package implementation to the ADL
+                    project. The source directory will be created inside the
+                    project directory""",
+                    default="src")
+
+parser.add_argument("-o", "--object-dir",
+                    help="""Name of the object output directory that will be used for your ADL project
+                    (default is obj). The object directory will be created
+                    inside the project directory""",
+                    default="obj")
+
+args = parser.parse_args()
+
+print description
+
+interactive = not args.script_mode
+use_default = not args.dont_use_defaults
+
+ADL_config = config.Database(interactive,
+                             use_default,
+                             config.user_input.console)
+
+if args.input_config is not None:
+    for f in args.input_config:
+        ADL_config.parse_input_config(f)
+
+if args.definitions is not None:
+    for d in args.definitions:
+        key, value = d.partition("=")[::2]
+        print "Add pre definition '%s' => '%s' from command line" % \
+            (key, value)
+
+        ADL_config.pre_define(key, value, "command line")
+
+ensure_dir(args.project_dir)
+
+ADL_configuration(ADL_config,
+                  os.path.join(cwd, args.project_dir),
+                  args.project_name,
+                  args.source_dir,
+                  args.object_dir)
