4cb48253557c4feecc9204ed016fc38a7a8fa36a
Fabien Chouteau <chouteau@adacore.com>
2018-07-11 19:07:41 +0200

    Travis-CI: Enable RISC-V32 project compilation

--- scripts/build_all_examples.py
+++ scripts/build_all_examples.py
@@ -5,7 +5,7 @@ import os
 import os.path
 import subprocess
 import sys
-
+import distutils.spawn
 
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
@@ -145,6 +145,12 @@ projects = [
             STM_DRIVERS + "/demo_usart_interrupts/demo_usart_interrupts.gpr",
             STM_DRIVERS + "/demo_usart_polling/demo_usart_polling.gpr",
             ]
+
+# Check if RISC-V32 compiler is available
+if distutils.spawn.find_executable("riscv32-elf-gnatls"):
+
+    # Add RISC-V32 projects
+    projects += ["/examples/HiFive1/hifive1_example.gpr"]
 
 parser = argparse.ArgumentParser('Compile all the Ada_Drivers_Library examples')
 
