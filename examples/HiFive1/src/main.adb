8dd5b437ff30c96cfd82b27fbe1bb25175984c81
Boran Car <boran.car@gmail.com>
2019-03-28 02:47:52 +0000

    Disable osc and spi settings for E300/X300
    E300/X300 is not exactly HiFive1, despite the identical memory
    addresses.

--- examples/HiFive1/src/main.adb
+++ examples/HiFive1/src/main.adb
@@ -42,15 +42,15 @@ begin
    --  The SPI flash clock divider should be as small as possible to increase
    --  the execution speed of instructions that are not yet in the instruction
    --  cache.
-   FE310.Set_SPI_Flash_Clock_Divider (2);
+   -- FE310.Set_SPI_Flash_Clock_Divider (2);
 
    --  Load the internal oscillator factory calibration to be sure it
    --  oscillates at a known frequency.
-   FE310.Load_Internal_Oscilator_Calibration;
+   -- FE310.Load_Internal_Oscilator_Calibration;
 
    --  Use the HiFive1 16 MHz crystal oscillator which is more acurate than the
    --  internal oscillator.
-   FE310.Use_Crystal_Oscillator;
+   -- FE310.Use_Crystal_Oscillator;
 
    HiFive1.LEDs.Initialize;
 
