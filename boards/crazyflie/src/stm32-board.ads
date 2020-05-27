a7ac9493934780e9ecf58fdf9b75e557aa99c1ed
Simon Wright <simon@pushface.org>
2020-05-27 11:18:37 +0100

    Add board feature to initialize the Crazyflie external SPI.
      * boards/crazyflie/src/stm32-board.ads (Initialize_EXT_SPI): new.
      * boards/crazyflie/src/stm32-board.adb (Initialize_EXT_SPI): new.

--- boards/crazyflie/src/stm32-board.ads
+++ boards/crazyflie/src/stm32-board.ads
@@ -1,6 +1,6 @@
 ------------------------------------------------------------------------------
 --                                                                          --
---                 Copyright (C) 2015-2016, AdaCore                         --
+--              Copyright (C) 2015-2016, 2020, AdaCore                      --
 --                                                                          --
 --  Redistribution and use in source and binary forms, with or without      --
 --  modification, are permitted provided that the following conditions are  --
@@ -130,6 +130,8 @@ package STM32.Board is
    ---------
    -- SPI --
    ---------
+
+   procedure Initialize_EXT_SPI;
 
    EXT_SPI  : SPI_Port renames SPI_1;
    EXT_SCK  : GPIO_Point renames PA5;
