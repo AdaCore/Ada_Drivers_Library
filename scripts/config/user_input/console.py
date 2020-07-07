9f88a65a31845c04a6c0931611547e5a2be371d6
Fabien Chouteau <Fabien-Chouteau@users.noreply.github.com>
2020-07-07 18:56:47 +0200

    scripts/config/user_input/console.py: Python3 migration
    raw_input() is renamed to input().
    
    Part of #354.

--- scripts/config/user_input/console.py
+++ scripts/config/user_input/console.py
@@ -33,7 +33,7 @@ def query_bool(question, default="yes"):
 
     while True:
         sys.stdout.write(question + prompt)
-        choice = raw_input().lower()
+        choice = input().lower()
         if choice == '?':
             continue
         elif default is not None and choice == '':
@@ -50,7 +50,7 @@ def query_string(question, default):
     prompt = " [default: '%s']\n? " % (default)
     while True:
         sys.stdout.write(question + prompt)
-        choice = raw_input()
+        choice = input()
         if choice == '?':
             continue
         elif default is not None and choice == '':
@@ -68,7 +68,7 @@ def query_choice(question, choices, default):
             print(" - (%d) %s" % (cnt, item))
             cnt += 1
         sys.stdout.write("? ")
-        choice = raw_input()
+        choice = input()
         if choice == '?':
             continue
         elif default is not None and choice == '':
@@ -99,7 +99,7 @@ def query_int(question, range_from, range_to, default):
 
     while True:
         sys.stdout.write(question + prompt)
-        choice = raw_input().lower()
+        choice = input().lower()
         if choice == '?':
             continue
         elif default is not None and choice == '':
@@ -131,7 +131,7 @@ def query_float(question, range_from, range_to, default):
 
     while True:
         sys.stdout.write(question + prompt)
-        choice = raw_input().lower()
+        choice = input().lower()
         if choice == '?':
             continue
         elif default is not None and choice == '':
