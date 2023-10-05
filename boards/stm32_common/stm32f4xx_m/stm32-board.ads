bec0e24cba01264ab54dd8c69adb82a5abf29cf9
Maxim Reznik <reznikmm@gmail.com>
2023-10-05 16:26:26 +0300

    Add STM32F4XX M DevEBox board
    https://stm32-base.org/boards/STM32F407VGT6-STM32F4XX-M

---
+++ boards/stm32_common/stm32f4xx_m/stm32-board.ads
@@ -0,0 +1,72 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                 Copyright (C) 2015-2023, AdaCore                         --
+--                                                                          --
+--  Redistribution and use in source and binary forms, with or without      --
+--  modification, are permitted provided that the following conditions are  --
+--  met:                                                                    --
+--     1. Redistributions of source code must retain the above copyright    --
+--        notice, this list of conditions and the following disclaimer.     --
+--     2. Redistributions in binary form must reproduce the above copyright --
+--        notice, this list of conditions and the following disclaimer in   --
+--        the documentation and/or other materials provided with the        --
+--        distribution.                                                     --
+--     3. Neither the name of STMicroelectronics nor the names of its       --
+--        contributors may be used to endorse or promote products derived   --
+--        from this software without specific prior written permission.     --
+--                                                                          --
+--   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    --
+--   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      --
+--   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR  --
+--   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT   --
+--   HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, --
+--   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       --
+--   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  --
+--   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  --
+--   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT    --
+--   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  --
+--   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   --
+--                                                                          --
+------------------------------------------------------------------------------
+
+--  This file provides declarations for devices on the STM32F4XX M board
+--  manufactured by DevEBox.
+
+with STM32.Device;  use STM32.Device;
+with STM32.GPIO;    use STM32.GPIO;
+
+with Ada.Interrupts.Names; use Ada.Interrupts;
+
+package STM32.Board is
+   pragma Elaborate_Body;
+
+   subtype User_LED is GPIO_Point;
+
+   Green_LED  : User_LED renames PA1;
+
+   All_LEDs : GPIO_Points := (1 => Green_LED);
+   LCH_LED  : GPIO_Point renames Green_LED;
+
+   procedure Initialize_LEDs;
+   --  MUST be called prior to any use of the LEDs
+
+   procedure Turn_On  (This : in out User_LED) renames STM32.GPIO.Set;
+   procedure Turn_Off (This : in out User_LED) renames STM32.GPIO.Clear;
+   procedure Toggle   (This : in out User_LED) renames STM32.GPIO.Toggle;
+
+   procedure All_LEDs_Off with Inline;
+   procedure All_LEDs_On  with Inline;
+   procedure Toggle_LEDs (These : in out GPIO_Points)
+     renames STM32.GPIO.Toggle;
+
+   --  User button
+
+   User_Button_Point     : GPIO_Point renames PA0;
+   User_Button_Interrupt : constant Interrupt_ID := Names.EXTI0_Interrupt;
+
+   procedure Configure_User_Button_GPIO;
+   --  Configures the GPIO port/pin for the K1 user button. Sufficient
+   --  for polling the button, and necessary for having the button generate
+   --  interrupts.
+
+end STM32.Board;
