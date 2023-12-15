8eb896f305ed2f6c638320ae5663e24568b15b3a
Maxim Reznik <reznikmm@gmail.com>
2023-12-15 17:29:09 +0200

    Add BME280 connected by SPI

---
+++ components/src/environment/bme280/bme280-spi.adb
@@ -0,0 +1,89 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                       Copyright (C) 2023, AdaCore                        --
+--                                                                          --
+--  Redistribution and use in source and binary forms, with or without      --
+--  modification, are permitted provided that the following conditions are  --
+--  met:                                                                    --
+--     1. Redistributions of source code must retain the above copyright    --
+--        notice, this list of conditions and the following disclaimer.     --
+--     2. Redistributions in binary form must reproduce the above copyright --
+--        notice, this list of conditions and the following disclaimer in   --
+--        the documentation and/or other materials provided with the        --
+--        distribution.                                                     --
+--     3. Neither the name of the copyright holder nor the names of its     --
+--        contributors may be used to endorse or promote products derived   --
+--        from this software without specific prior written permission.     --
+--                                                                          --
+--   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    --
+--   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      --
+--   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR  --
+--   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT   --
+--   HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, --
+--   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       --
+--   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  --
+--   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  --
+--   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT    --
+--   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  --
+--   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   --
+--                                                                          --
+------------------------------------------------------------------------------
+
+package body BME280.SPI is
+
+   ----------
+   -- Read --
+   ----------
+
+   procedure Read
+     (Data    : out HAL.UInt8_Array;
+      Success : out Boolean)
+   is
+      use type HAL.UInt8;
+      use all type HAL.SPI.SPI_Status;
+
+      Addr : HAL.UInt8;
+      Status : HAL.SPI.SPI_Status;
+   begin
+      SPI.SPI_CS.Clear;
+
+      Addr := HAL.UInt8 (Data'First) or 16#80#;
+      SPI_Port.Transmit (HAL.SPI.SPI_Data_8b'(1 => Addr), Status);
+
+      if Status = Ok then
+         SPI_Port.Receive (HAL.SPI.SPI_Data_8b (Data), Status);
+      end if;
+
+      SPI.SPI_CS.Set;
+
+      Success := Status = Ok;
+   end Read;
+
+   -----------
+   -- Write --
+   -----------
+
+   procedure Write
+     (Data    : HAL.UInt8_Array;
+      Success : out Boolean)
+   is
+      use type HAL.UInt8;
+      use all type HAL.SPI.SPI_Status;
+
+      Addr : HAL.UInt8;
+      Status : HAL.SPI.SPI_Status;
+   begin
+      SPI.SPI_CS.Clear;
+
+      for J in Data'Range loop
+         Addr := HAL.UInt8 (J) and 16#7F#;
+         SPI_Port.Transmit (HAL.SPI.SPI_Data_8b'(Addr, Data (J)), Status);
+         exit when Status /= Ok;
+      end loop;
+
+      SPI.SPI_CS.Set;
+
+      Success := Status = Ok;
+   end Write;
+
+end BME280.SPI;
---
+++ components/src/environment/bme280/bme280-spi.ads
@@ -0,0 +1,54 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                       Copyright (C) 2023, AdaCore                        --
+--                                                                          --
+--  Redistribution and use in source and binary forms, with or without      --
+--  modification, are permitted provided that the following conditions are  --
+--  met:                                                                    --
+--     1. Redistributions of source code must retain the above copyright    --
+--        notice, this list of conditions and the following disclaimer.     --
+--     2. Redistributions in binary form must reproduce the above copyright --
+--        notice, this list of conditions and the following disclaimer in   --
+--        the documentation and/or other materials provided with the        --
+--        distribution.                                                     --
+--     3. Neither the name of the copyright holder nor the names of its     --
+--        contributors may be used to endorse or promote products derived   --
+--        from this software without specific prior written permission.     --
+--                                                                          --
+--   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    --
+--   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      --
+--   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR  --
+--   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT   --
+--   HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, --
+--   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       --
+--   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  --
+--   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  --
+--   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT    --
+--   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  --
+--   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   --
+--                                                                          --
+------------------------------------------------------------------------------
+
+--  This package provides an easy way to setup BME280 connected via SPI.
+
+with HAL.GPIO;
+with HAL.SPI;
+
+generic
+   SPI_Port : not null HAL.SPI.Any_SPI_Port;
+   SPI_CS   : not null HAL.GPIO.Any_GPIO_Point;
+package BME280.SPI is
+
+   procedure Read
+     (Data    : out HAL.UInt8_Array;
+      Success : out Boolean);
+   --  Read registers starting from Data'First
+
+   procedure Write
+     (Data    : HAL.UInt8_Array;
+      Success : out Boolean);
+   --  Write registers starting from Data'First
+
+   package Sensor is new Generic_Sensor (Read, Write);
+
+end BME280.SPI;
