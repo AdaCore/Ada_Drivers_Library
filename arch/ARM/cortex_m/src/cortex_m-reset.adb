811096d883232687fc363fbbe1fdb244f6e31bb3
Maxim Reznik <reznikmm@gmail.com>
2024-10-28 20:16:09 +0200

    Add System Reset Request routine.

---
+++ arch/ARM/cortex_m/src/cortex_m-reset.adb
@@ -0,0 +1,49 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                        Copyright (C) 2024, AdaCore                       --
+--                                                                          --
+--  Redistribution and use in source and binary forms, with or without      --
+--  modification, are permitted provided that the following conditions are  --
+--  met:                                                                    --
+--     1. Redistributions of source code must retain the above copyright    --
+--        notice, this list of conditions and the following disclaimer.     --
+--     2. Redistributions in binary form must reproduce the above copyright --
+--        notice, this list of conditions and the following disclaimer in   --
+--        the documentation and/or other materials provided with the        --
+--        distribution.                                                     --
+--     3. Neither the name of the copyright holder nor the names of its     --
+--        contributors may be used to endorse or promote products derived   --
+--        from this software without specific prior written permission.     --
+--                                                                          --
+--   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    --
+--   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      --
+--   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR  --
+--   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT   --
+--   HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, --
+--   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       --
+--   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  --
+--   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  --
+--   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT    --
+--   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  --
+--   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with Memory_Barriers;
+with Cortex_M_SVD.SCB;
+
+procedure Cortex_M.Reset is
+begin
+   Memory_Barriers.Data_Synchronization_Barrier;
+
+   Cortex_M_SVD.SCB.SCB_Periph.AIRCR :=
+     (SYSRESETREQ => True,
+      VECTKEY     => Cortex_M_SVD.SCB.Key,
+      others      => <>);
+
+   Memory_Barriers.Data_Synchronization_Barrier;
+
+   loop
+      null;
+   end loop;
+end Cortex_M.Reset;
